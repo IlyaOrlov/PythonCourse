693fc3cde15a72cd38bb59770fde6585ee96a208
kirilldotsenko <44436596+kirilldotsenko@users.noreply.github.com>
2018-10-30 21:37:38 +0300

    Add solution for task 1. (#2)
    * My task 1
    
    * My task 1

--- Practice/Kirill Dotsenko/2zadanie.py
+++ Practice/Kirill Dotsenko/2zadanie.py
@@ -1,10 +1,4 @@
-a=int(input("Vvedite chislo:"))
-c=[]
-while a%10!=0:
-	b=int(a%10)
-	c.append(b)
-	#print(str(b)+"цифра равна"+str(b))
-	a=(a-b)/10
-c.reverse()
-for e in c:
-	print(str(e)+" цифра равна "+str(e))
+a=input("Vvedite chislo:")
+s=list(a)
+for k in a:
+	print(k+" цифра равна "+k)
