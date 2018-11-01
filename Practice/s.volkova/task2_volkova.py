93c4d07da6e7bb32690c37beabfb886bc8d893d6
s-volkova <volkova.san@gmail.com>
2018-11-01 11:24:57 +0300

    Tasks 1-3

---
+++ Practice/s.volkova/task2_volkova.py
@@ -0,0 +1,13 @@
+#for Python 3.6
+#task 2
+
+def number_reader(x):
+    if x.isdigit() and len(x) == 5:
+        for i in range(5):
+            print("{} цифра равна {}".format(i+1, x[i]))
+    else:
+        print("Это не пятизначное число")
+
+
+if __name__ == '__main__':
+    number_reader(input("Введите пятизначное число:\n"))
