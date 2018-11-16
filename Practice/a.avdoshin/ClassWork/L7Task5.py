12a689c04c12dc57304c1ecda7391dfda177e079
hydrazine114 <hydrazine114@gmail.com>
2018-11-16 07:52:16 +0300

    test

--- Practice/a.avdoshin/ClassWork/L7Task5.py
+++
@@ -1,9 +0,0 @@
-def count_symbol(input_str, symbol):
-    count = 0
-    for i in range(len(input_str) - len(symbol)):
-        if input_str[i:i + len(symbol)] == symbol:
-            count += 1
-    return count
-
-
-print(count_symbol('Good by my friend, my only friend', 'my'))
