988d1f3285be50cf99cbaf8caa1300418b6882db
Alex <hydrazine114@gmail.com>
2018-11-24 00:47:28 +0300

    Correct

--- Practice/a.avdoshin/ClassWork/L7Task4.py
+++ Practice/a.avdoshin/ClassWork/L7Task4.py
@@ -5,12 +5,12 @@ import re
 def to_title(input_str):
     input_str = input_str[0].upper() + input_str[1:]
     words = re.split('[\s]', input_str)
-    result = ''
-    for word in words:
+    result = words[0]
+    for word in words[1:]:
         result += ' ' + word[0].upper() + word[1:]
     # 22.11 - [ИО]:  добавлять лишний пробел, а потом его убирать
     # - неэффективно
-    return result[1:]
+    return result
 
 
 print(to_title('orlov Ilya evgen'))
