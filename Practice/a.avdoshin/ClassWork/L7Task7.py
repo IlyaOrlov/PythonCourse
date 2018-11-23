a00902f8aae48a5437d21ac2f7411bfe6335cb56
Alex <hydrazine114@gmail.com>
2018-11-24 00:14:14 +0300

    Correct

--- Practice/a.avdoshin/ClassWork/L7Task7.py
+++ Practice/a.avdoshin/ClassWork/L7Task7.py
@@ -7,9 +7,9 @@ def copyFile(source, distanation):
         raise FileExistsError
     # 22.11 - [ИО]:  работать с файлами без менеджера контекста неправильно,
     # а не закрывать их после работы вообще преступление)
-    origin = open(source, 'rb')
-    copy = open(distanation, 'wb')
-    copy.write(origin.read())
+    with open(source, 'rb') as origin, open(distanation, 'wb') as copy:
+        copy.write(origin.read())
+
 
 if __name__ == '__main__':
-    copyFile('TestFile.txt', 'copyTest')
+    copyFile('TestFile.txt', 'copyTest.txt')
