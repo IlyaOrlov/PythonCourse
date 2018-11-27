c464f5d63c41dc9cfce106b6e3d9fa5a21d1b141
orlov.soft.company@gmail.com <orlov@mera.ru>
2018-11-27 17:51:43 +0300

    Lecture 10 and Scripts directory

---
+++ Lectures/Scripts/Lec_10/celery_consumer.py
@@ -0,0 +1,44 @@
+import time
+from celery import Celery
+
+
+app = Celery('celery_consumer', broker='amqp://guest@localhost//')
+app.conf.CELERY_RESULT_BACKEND = 'amqp://'
+#app.conf.update(
+#    CELERY_RESULT_BACKEND='amqp://',
+#)
+
+@app.task
+def add(x,y):
+    print('add({0}, {1})'.format(x, y))
+    time.sleep(0.1)
+    return x + y
+
+@app.task
+def mul(x,y):
+    print('mul({0}, {1})'.format(x, y))
+    return x * y
+
+@app.task
+def xsum(l):
+    print('xsum({0})'.format(l))
+    return sum(l)
+
+@app.task
+def broken_task():
+    raise Exception('failed')
+
+@app.task
+def on_task_error(task_id, task_name):
+    print('ERROR IN TASK {0}: {1}'.format(task_id, task_name))
+
+@app.task
+def print_task(x):
+    print(x)
+
+@app.task
+def on_task_success(task_id, task_name):
+    print('TASK SUCCEDED {0}: {1}'.format(task_id, task_name))
+
+if __name__ == '__main__':
+    app.worker_main()
