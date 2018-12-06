2b84771f7254871d07433c9dc576380dd9a81f97
IlyaOrlov <orlov@mera.ru>
2018-12-06 18:27:37 +0300

    Add files via upload

---
+++ Lectures/Scripts/Lec_12/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='edurequesthandler',
+      version='0.0.1',
+      description='Handler for education requests',
+      author='Orlov Ilya',
+      author_email='orlov@mera.ru',
+      packages=['edurequesthandler']
+      )
