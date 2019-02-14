18311e66162ecbe6f09e2b9d1586db30e03b20b8
Fedotova <elena.fedotova@intel.com>
2019-02-14 11:04:14 +0300

    Задания к лекции 2

---
+++ Practice/e_fedotova/test3.py
@@ -0,0 +1,23 @@
+# задание 1
+def function(a, b):
+    if a < b:
+        print(b)
+    elif a > b:
+        print(a)
+    else:
+        print('Числа должны быть разными')
+
+function(3, 5)
+
+# задание 2
+def function_one(a, b):
+    if a < b:
+        return b
+    elif a > b:
+        return a
+    else:
+        print('Числа должны быть разными')
+
+function_one(7, 8)
+
+# задание 3
