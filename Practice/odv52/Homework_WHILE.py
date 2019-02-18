263e3c4e315601f500ba9cb88d4d16c0f0e89dee
odv52 <odv@52.ru>
2019-02-18 18:28:02 +0300

    homework

---
+++ Practice/odv52/Homework_WHILE.py
@@ -0,0 +1,7 @@
+cmd = input('Введите команду для выхода из консоли Python : ')
+while cmd != 'exit':
+    print('Не верно!')
+    cmd = input('Введите команду для выхода из консоли Python : ')
+    continue
+else:
+    print('ВЕРНО!')
