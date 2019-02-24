8925433e826381ed5c6d94fe6a9bfd60314ed1b5
ATseretyan123 <47469835+ATseretyan123@users.noreply.github.com>
2019-02-24 09:23:40 +0300

    задание2

---
+++ Practice/aramtseretyan/задание2.py
@@ -0,0 +1,15 @@
+
+# задание 2
+def function (a, b):
+    if a > b:
+        print (a)
+    elif a < b:
+	print (b)
+
+
+# задание 3
+def function (a, b):
+    if a > b:
+       return a
+    elif a < b:
+       return b
