8e550a1d3b6b7972de95ad9453aece8c56572550
timofeichev <a.timofeichev@yandex.ru>
2019-02-26 15:00:01 +0400

    da

--- Practice/timofeichevHomeWork/timofeichevPracticeLec4.py
+++ Practice/timofeichevHomeWork/timofeichevPracticeLec4.py
@@ -3,28 +3,33 @@
 l = range(1, 101)
 
 def test(l):
-
-    for i in (l):
-        if i % 3 == 0:
+    for i in l:
+        if i % 15 == 0:
+            i = 'FizzBuzz'
+        elif i % 3 == 0:
             i = 'Fizz'
-
         elif i % 5 == 0:
             i = 'Buzz'
-
-        elif i % 15 == 0:
-            i = 'FizzBuzz'
         print(i)
 test(l)
 
-
 #Задание №2
 
-a = int(10819)
-b = str(a)
-c = 0
-for i in (b):
-    c += 1
-    print('{} цифра равна {}'.format(c, i))
+a = input('Введите 5-ти значное число:')
+while a.isdigit():
+    if len(a) == 5:
+        c = 0
+        for i in a:
+            c += 1
+            print('{} цифра равна {}'.format(c, i))
+        break
+    else:
+        print('Вы ввели не 5-ти значное число')
+        a = input('Введите 5-ти значное число:')
+else:
+    print("Вы ввели не число")
+
+
 
 
 
