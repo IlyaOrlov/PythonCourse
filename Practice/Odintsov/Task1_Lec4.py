b5b3199db61457110d763cf1b4917bf62ede3d41
ZigZagNN <odintsov_alexander_nn@mail.ru>
2019-02-28 00:24:59 +0300

    первые_три_задания_к_4_лекции

---
+++ Practice/Odintsov/Task1_Lec4.py
@@ -0,0 +1,12 @@
+l = [x for x in range(101)]
+l.pop(0)
+
+for i in l:
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
