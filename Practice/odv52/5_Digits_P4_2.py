a47a0176faa45e3938f29b262dd25656457e5224
odv52 <odv@52.ru>
2019-03-01 12:17:52 +0300

    Add files via upload
    Задание 2 к лекции № 4

---
+++ Practice/odv52/5_Digits_P4_2.py
@@ -0,0 +1,8 @@
+dgts = input ("введите пятизначное число:")
+a, b, c, d, e = dgts
+keyword = "цифра равна"
+print("1", keyword, a)
+print("2", keyword, b)
+print("3", keyword, c)
+print("4", keyword, d)
+print("5", keyword, e)
