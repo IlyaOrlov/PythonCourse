9774c3e49b655165f352aa8ea5c0f34e9c1672f0
timofeichev <47556252+timofeichev@users.noreply.github.com>
2019-03-03 21:50:55 +0300

    Timofeichev (#29)
    * comment
    
    * Update and rename Practice/timofeichevHomeWork/timofeichev_practice1.py to  Практика/ timofeichevHomeWork/timofeichev_practice1.py
    
    * Delete timofeichev_practice1.py
    
    * comment1
    
    * first comment
    
    * comment37436
    
    * da
    
    * hi

--- Practice/timofeichevHomeWork/timofeichevPracticeLec4.py
+++ Practice/timofeichevHomeWork/timofeichevPracticeLec4.py
@@ -1,4 +1,4 @@
-#Задание №1
+# Задание №1
 
 l = range(1, 101)
 
@@ -10,10 +10,11 @@ def test(l):
             i = 'Fizz'
         elif i % 5 == 0:
             i = 'Buzz'
-        print(i)
+        print(i, end = ',')
 test(l)
 
-#Задание №2
+
+# Задание №2
 
 a = input('Введите 5-ти значное число:')
 while a.isdigit():
@@ -30,9 +31,32 @@ else:
     print("Вы ввели не число")
 
 
+# Задание №3
+
+# Рекурсия
+
+s = [0, 3, 24, 2, 3, 7]
+l = []
+def test(s, l):
+    if len(s) != 0:
+        a = s.index(min(s))
+        l.append(s[a])
+        del s[a]
+        test(s, l)
+    else:
+        print('Список отсортирован:{}'.format(l))
+test(s, l)
 
 
+# Цикл while
 
-
-
-
+s = [0, 3, 24, 2, 3, 7]
+l = []
+def test(s, l):
+    while len(s) != 0:
+        a = s.index(min(s))
+        l.append(s[a])
+        del s[a]
+    else:
+        print('Список отсортирован:{}'.format(l))
+test(s, l)
