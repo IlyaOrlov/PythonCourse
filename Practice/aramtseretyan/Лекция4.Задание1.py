e74c7a49962d49ee5808e739311f61eda75cb185
ATseretyan123 <47469835+ATseretyan123@users.noreply.github.com>
2019-03-03 16:54:29 +0300

    Лекция4.Задание1

---
+++ Practice/aramtseretyan/Лекция4.Задание1.py
@@ -0,0 +1,19 @@
+a = range(1, 101)
+b = 'Buzz'
+c = 'Fizz'
+d = 'FizzBuzz'
+
+def work(a):
+    for i in a :
+        if i % 3 ==0 :
+            i = b
+        elif i % 5 ==0:
+            i = c
+        elif i % 15:
+            i == d
+        print(i)
+work(a)
+            
+
+
+
