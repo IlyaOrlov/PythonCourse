ff2379a191e061260deeb923ded447c3db959b7d
odv52 <odv@52.ru>
2019-03-06 12:49:44 +0300

    Update 5_Digits_P4_2.py

--- Practice/odv52/5_Digits_P4_2.py
+++ Practice/odv52/5_Digits_P4_2.py
@@ -1,8 +1,3 @@
-dgts = input ("введите пятизначное число:")
-a, b, c, d, e = dgts
-keyword = "цифра равна"
-print("1", keyword, a)
-print("2", keyword, b)
-print("3", keyword, c)
-print("4", keyword, d)
-print("5", keyword, e)
+digs = (input("Введите цифры:"))
+for i, x in enumerate(digs):
+    print("%d цифра равна %s" % (i, x))
