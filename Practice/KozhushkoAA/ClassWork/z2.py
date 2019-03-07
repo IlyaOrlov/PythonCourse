c9f9d1adbcefa22ee0704f19e61ed7239c2180e4
Andrew04071989 <Kozhuzha@gmail.com>
2019-03-07 20:02:52 +0300

    Лекция 8, задания 2(исправленное)

--- Practice/KozhushkoAA/ClassWork/z2.py
+++ Practice/KozhushkoAA/ClassWork/z2.py
@@ -1,16 +1,15 @@
-s = [1, 1, 545, 888, 999, 777, 66, 1000, 888, 6, 8]
+s = [1, 77, 999, 777, 545, 888]
 
 
 def revers(s):
     n = len(s) / 2
     i = 0
     j = len(s) - 1
-    while i <= n:
-        while j >= n:
-            s[i], s[j] = s[j], s[i]
-            j -= 1
-            i += 1
-        return(s)
+    while i < n:
+        s[i], s[j] = s[j], s[i]
+        j -= 1
+        i += 1
+    return(s)
 
 
 print(revers(s))
