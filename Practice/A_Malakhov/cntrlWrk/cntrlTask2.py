f8f0cd47c87a928de9e836796a6ea91bfc015937
AlexandrMalahov <knessdar2@gmail.com>
2019-03-07 03:30:30 +0300

    Create cntrlTask2.py

---
+++ Practice/A_Malakhov/cntrlWrk/cntrlTask2.py
@@ -0,0 +1,11 @@
+def reverse1():
+    l = [1, 2, 3, 4, 5]
+    i = 0
+    print(l)
+    while i < len(l) / 2:
+        l[i], l[len(l) - i - 1] = l[len(l) - i - 1], l[i]
+        i += 1
+    print(l)
+
+
+reverse1()
