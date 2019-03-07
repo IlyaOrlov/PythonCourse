64298a539fea6a775617b8ef9d6cc0ec69d12425
AlexandrMalahov <knessdar2@gmail.com>
2019-03-07 03:22:35 +0300

    Create task1.py

---
+++ Practice/A_Malakhov/Lec2/task1.py
@@ -0,0 +1,8 @@
+def task1(n, m):
+    if n > m:
+        print(n)
+    else:
+        print(m)
+
+
+task1(n = int(input()), m = int(input()))
---
+++ Practice/A_Malakhov/Lec5/task1Lec5.py
@@ -0,0 +1,10 @@
+class Man:
+    def __init__(self, name):
+        self.name = name
+
+    def solve_task(self):
+        print("I'm not ready yet")
+
+
+n = Man('John')
+n.solve_task()
