ab9508f1a7b9035a102c7eafc70d2c23825f4e1d
Andrew04071989 <Kozhuzha@gmail.com>
2019-03-14 00:11:54 +0300

    changes for Lec4,z4_file

--- Practice/KozhushkoAA/HWlec4/HWlec4_z4_file.py
+++ Practice/KozhushkoAA/HWlec4/HWlec4_z4_file.py
@@ -1,9 +1,16 @@
-#Замена пробелов на табы в файле
-f = open("SpcOnTab.txt", 'w')
-f.write('Кожушко    Андрей    Андреевич')
-f.close()
-f = open("tab&str.txt", 'r+')
-a = f.readlines()
+# Замена пробелов на табы в файле
+# f = open("SpcOnTab.txt", 'w')
+# f.write('    Кожушко    Андрей    Андреевич    ')
+# f.close()
+# f = open("SpcOnTab.txt", 'r+')
+# a = f.readlines()
+# Согласен, что менеджер контекста удобнее,
+# но это задание к 4 лекции, где менеджер контекста ещё не был изучен.
+with open("SpcOnTab.txt", 'w') as f:
+    f.write('    Кожушко    Андрей    Андреевич    ')
+with open("SpcOnTab.txt", 'r+') as f:
+    a = f.readlines()
+
 
 def space_and_tab(a):
     a = list(a)
@@ -19,16 +26,22 @@ def space_and_tab(a):
     a = x.join(a)
     print(a)
 
+
 space_and_tab(a)
-f.close()
+# f.close()
 
 
 #Замена табов на прбелы в файле
-f = open("TabOnSpc.txt", 'w')
-f.write('Кожушко\tАндрей\tАндреевич')
-f.close()
-f = open("tab&str.txt", 'r+')
-a = f.readline()
+#f = open("TabOnSpc.txt", 'w')
+#f.write('\tКожушко\tАндрей\tАндреевич\t')
+#f.close()
+#f = open("TabOnSpc.txt", 'r+')
+#a = f.readline()
+with open("TabOnSpc.txt", 'w') as f:
+    f.write('\tКожушко\tАндрей\tАндреевич\t')
+with open("TabOnSpc.txt", 'r+') as f:
+    a = f.readlines()
+
 
 def space_and_tab(a):
     a = list(a)
@@ -37,6 +50,9 @@ def space_and_tab(a):
         if a[i] == '\t':
             a[i] = '    '
         elif a[i:(i+4)] == [' ', ' ', ' ', ' ']:
+# Да, сработает, если имеется ввиду именно 4 пробела
+# "    Иванов    Иван    Иванович    ".
+# С одним пробелом работать и не должно.
             del a[(i+1):(i+4)]
             a[i] = '\t'
         i+= 1
@@ -44,5 +60,10 @@ def space_and_tab(a):
     a = x.join(a)
     print(a)
 
+
 space_and_tab(a)
-f.close()
+# f.close()
+
+# Также нашел ошибку: Имя файла открытого для 'w'
+# не соответствовало имени файла открытого для 'r+'
+# Исправил
