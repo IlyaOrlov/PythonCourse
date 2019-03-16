6bf1ae3ca84b6ce8f63146c8b9b500cc7ddad604
Andrew04071989 <Kozhuzha@gmail.com>
2019-03-16 13:42:34 +0300

    changes for Lec4,z4_file

--- Practice/KozhushkoAA/HWlec4/HWlec4_z4_file.py
+++ Practice/KozhushkoAA/HWlec4/HWlec4_z4_file.py
@@ -1,15 +1,8 @@
 # Замена пробелов на табы в файле
-# f = open("SpcOnTab.txt", 'w')
-# f.write('    Кожушко    Андрей    Андреевич    ')
-# f.close()
-# f = open("SpcOnTab.txt", 'r+')
-# a = f.readlines()
-# Согласен, что менеджер контекста удобнее,
-# но это задание к 4 лекции, где менеджер контекста ещё не был изучен.
 with open("SpcOnTab.txt", 'w') as f:
     f.write('    Кожушко    Андрей    Андреевич    ')
 with open("SpcOnTab.txt", 'r+') as f:
-    a = f.readlines()
+    a = f.readline()
 
 
 def space_and_tab(a):
@@ -21,49 +14,24 @@ def space_and_tab(a):
         elif a[i:(i+4)] == [' ', ' ', ' ', ' ']:
             del a[(i+1):(i+4)]
             a[i] = '\t'
-        i+= 1
+        i += 1
     x = ''
     a = x.join(a)
     print(a)
 
 
 space_and_tab(a)
-# f.close()
-
 
 #Замена табов на прбелы в файле
-#f = open("TabOnSpc.txt", 'w')
-#f.write('\tКожушко\tАндрей\tАндреевич\t')
-#f.close()
-#f = open("TabOnSpc.txt", 'r+')
-#a = f.readline()
 with open("TabOnSpc.txt", 'w') as f:
     f.write('\tКожушко\tАндрей\tАндреевич\t')
 with open("TabOnSpc.txt", 'r+') as f:
-    a = f.readlines()
-
-
-def space_and_tab(a):
-    a = list(a)
-    i = 0
-    while i < len(a):
-        if a[i] == '\t':
-            a[i] = '    '
-        elif a[i:(i+4)] == [' ', ' ', ' ', ' ']:
-# Да, сработает, если имеется ввиду именно 4 пробела
-# "    Иванов    Иван    Иванович    ".
-# С одним пробелом работать и не должно.
-            del a[(i+1):(i+4)]
-            a[i] = '\t'
-        i+= 1
-    x = ''
-    a = x.join(a)
-    print(a)
+    a = f.readline()
 
 
 space_and_tab(a)
-# f.close()
-
-# Также нашел ошибку: Имя файла открытого для 'w'
-# не соответствовало имени файла открытого для 'r+'
-# Исправил
+# Переписал программу:
+# 1) a = f.readlines() исправлено на a = f.readline(), теперь
+#    "a" получается списком из нескольких элементов;
+# 2) Согласен, два раза определять функцию незачем, поэтому для
+#    второго случая функция просто вызывается.
