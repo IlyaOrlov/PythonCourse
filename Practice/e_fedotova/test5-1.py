692bd943c463f464c87c420c1cf8f3d8c35f809f
Fedotova <elena.fedotova@intel.com>
2019-03-26 16:32:52 +0300

    1е задание, 5 лекция

---
+++ Practice/e_fedotova/test5-1.py
@@ -0,0 +1,11 @@
+
+class Man:
+    def __init__(self, n):
+        self.name = n
+
+    def solve_task(self):
+        return "not ready yet"
+
+boy = Man("Alex")
+status = boy.solve_task()
+print(boy.name, status)
