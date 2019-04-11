1bd5a9dc01a4cb23634cf1c4a710e91e846ee880
AlexandrMalahov <knessdar2@gmail.com>
2019-04-11 19:18:53 +0300

    Update task2_exam.py

--- Practice/A_Malakhov/Exam/task2_exam.py
+++ Practice/A_Malakhov/Exam/task2_exam.py
@@ -1,16 +1,16 @@
 class Iteration:
-    lst = []
+
     def __init__(self, n):
         self.n = n
+        self.lst = []
         self.k = 1
-
 
     def __iter__(self):
         return self
 
     def __next__(self):
         a = 2
-        while self.k < self.n + 1:
+        while self.k <= self.n + 1:
             is_prime = False
             for i in self.lst:
                 if a % i == 0:
@@ -19,12 +19,11 @@ class Iteration:
             if is_prime == False:
                 self.k += 1
                 self.lst.append(a)
-                yield a
+                return a
             a += 1
+            if self.k == self.n + 1:
+                raise StopIteration
 
 
-try:
-    for i in Iteration(20):
-        print(next(i), end=' ')
-except StopIteration:
-    print('')
+for i in Iteration(20):
+    print(i, end=' ')
