43b55716f70edf45611a945cf46c20d3dbc27ba5
AlexandrMonakhov <zabuldigaa@gmail.com>
2019-06-04 22:19:03 +0300

    Add Task5.py and ./Shalagin

---
+++ Practice/Shalagin/TabToSpace.py
@@ -0,0 +1,14 @@
+def changeStr(line,s1,s2):
+    print(line)
+    beg = line.find(s1)
+    while not beg == -1:
+        line = line[:beg] + s2 + line[beg + 1:]
+        beg = line.find(s1)
+        print(line)
+
+
+line="keys:\tvalue:"
+s1="\t"
+s2="    "
+changeStr(line,s1,s2)
+print(line.replace(s1,s2))
