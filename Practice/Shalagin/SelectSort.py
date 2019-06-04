06f4f571b7aca5ee9beac81abece2e4fdf3c0d4c
AlexandrMonakhov <zabuldigaa@gmail.com>
2019-06-04 13:56:12 +0300

    del 'shalagin'

--- Practice/Shalagin/SelectSort.py
+++
@@ -1,8 +0,0 @@
-arr=[0,3,24,2,3,7]
-i=0
-while i < len(arr):
-    minElement = min(arr[i:])
-    minIndex = arr[i:].index(minElement) + i
-    arr[i], arr[minIndex] = arr[minIndex], arr[i]
-    i+=1
-print(arr)
