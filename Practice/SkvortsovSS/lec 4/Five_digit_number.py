c64db4aa92551562fb71ca645f96ce8e27a5b180
Skvortsovss <skvortsovss@mail.ru>
2019-06-18 12:04:48 +0300

    SkvortsovSS_lec_8

--- Practice/SkvortsovSS/lec 4/Five_digit_number.py
+++
@@ -1,9 +0,0 @@
-def five_digit_number():
-    print("enter a five-digit number")
-    a = list(input())
-    k=1
-    for i in a:
-        print(k,"digit is equal to", i)
-        k+=1
-
-five_digit_number()
