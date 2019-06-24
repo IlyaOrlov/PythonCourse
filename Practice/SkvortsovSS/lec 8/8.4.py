be88e255e2c2934ce5df5a22f3271a9926afe76b
Sergey <44648715+Skvortsovss@users.noreply.github.com>
2019-06-25 01:24:13 +0300

    Skvortsov lec 8 (#50)
    * SkvortsovSS pract 4, 6, 8
    
    * SkvortsovSS_lec_8

---
+++ Practice/SkvortsovSS/lec 8/8.4.py
@@ -0,0 +1,11 @@
+def func(st):
+    a = ' '
+    d = []
+    c = st.split(' ')
+    print(c)
+    for i in c:
+       d.append(i.capitalize())
+    b = a.join(d)
+    print(b)
+
+func('erG rgr gtr')
