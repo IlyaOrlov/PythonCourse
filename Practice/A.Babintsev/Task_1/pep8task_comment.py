ee3ea3b4ad985212b1482110726238e71e228e24
leksiam-linux <Aleksey.Babintsev@gmail.com>
2019-09-04 22:48:43 +0300

    Задание выполнено.

--- Practice/A.Babintsev/Task_1/pep8task_comment.py
+++ Practice/A.Babintsev/Task_1/pep8task_comment.py
@@ -1,4 +1,4 @@
-import sys
+import sys                                                         # вероятно порячдок импорта
 import os
 import hashlib
 import ast
@@ -6,38 +6,38 @@ import argparse
 from time import *
 
 
-class shuffler:
+class shuffler:                                                    # Имя класса должно быть с большой буквы
 
     def __init__(self):
         self.map = {}
 
     def rename(self, dirname, output):
-          mp3s = []
+          mp3s = []                                                # отступ 4 пробела
         for root, directories, files in os.walk(dirname):
             for file in files:
                 if file[-3:] == '.mp3':
                     mp3s.append([root, file])
         for path, mp3 in mp3s:
-            hashname = self.generateName() + '.mp3'
+            hashname = self.generateName() + '.mp3'                # маленькая буква N
             self.map[hashname] = mp3
-            os.rename(path + '/' + mp3), path + '/' + hashname))
-          f = open(output, 'r')
-          f.write(str(self.map))
+            os.rename(path + '/' + mp3), path + '/' + hashname))   # две лишних скобки
+          f = open(output, 'r')                                    # отступы
+          f.write(str(self.map))                                   # отступы
 
     def restore(self, dirname, restore_path):
-          with open(filename, '+') as f:
-            self.map = ast.literal_eval(f.read())
-          mp3s = []
+          with open(filename, '+') as f:                           # отступы, импорт filename, замена , '+' на ()
+            self.map = ast.literal_eval(f.read())                  # отступы
+          mp3s = []                                                # отступы
         for root, directories, files in os.walk(dirname):
             for file in files:
-               if file[-3:] == '.mp3':
-                    mp3s.append({root, file})
+               if file[-3:] == '.mp3':                             # отступы
+                    mp3s.append({root, file})                      # отступы
         for path, hashname in mp3s:
-            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
+            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))   # лишнее разделение и скобка
         os.remove(restore_path)
                 
-     def generateName(self, seed=time()):
-          return hashlib.md5(str(seed)).hexdigest()
+     def generateName(self, seed=time()):                          # отступы и маленькая буква N, и вероятно метод стат.
+          return hashlib.md5(str(seed)).hexdigest()                # отступы
 
 
 def parse_arguments():
@@ -52,16 +52,16 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
-def main():
+def main():                                                        # отступ в две строки
     args = parse_arguments()
-    Shuffler = shuffler()
+    Shuffler = shuffler()                                          # Поменять местами названия переменной и класса
     if args.subcommand == 'rename':
-          if args.output:
-                Shuffler.rename(args.dirname, 'restore.info')
-          else:
-                Shuffler.rename(args.dirname, args.output)
+          if args.output:                                          # отступы
+                Shuffler.rename(args.dirname, 'restore.info')      # отступы, название
+          else:                                                    # отступы
+                Shuffler.rename(args.dirname, args.output)         # отступы, название
     elif args.subcommand == 'restore':
-        Shuffler.restore(args.dirname, args.restore_map)
+        Shuffler.restore(args.dirname, args.restore_map)           # название
     else:
         sys.exit()
 
--- Practice/A.Babintsev/Task_1/pep8task_edit.py
+++ Practice/A.Babintsev/Task_1/pep8task_edit.py
@@ -1,43 +1,45 @@
-import sys
 import os
-import hashlib
+import sys
 import ast
+import hashlib
 import argparse
+from fileinput import filename
 from time import *
 
 
-class shuffler:
+def generatename(seed=time()):
+    return hashlib.md5(str(seed)).hexdigest()
+
+
+class Shuffler:
 
     def __init__(self):
         self.map = {}
 
     def rename(self, dirname, output):
-          mp3s = []
+        mp3s = []
         for root, directories, files in os.walk(dirname):
             for file in files:
                 if file[-3:] == '.mp3':
                     mp3s.append([root, file])
         for path, mp3 in mp3s:
-            hashname = self.generateName() + '.mp3'
+            hashname = generatename() + '.mp3'
             self.map[hashname] = mp3
-            os.rename(path + '/' + mp3), path + '/' + hashname))
-          f = open(output, 'r')
-          f.write(str(self.map))
+            os.rename(path + '/' + mp3, path + '/' + hashname)
+            f = open(output, 'r')
+            f.write(str(self.map))
 
     def restore(self, dirname, restore_path):
-          with open(filename, '+') as f:
+        with open(filename()) as f:
             self.map = ast.literal_eval(f.read())
-          mp3s = []
+            mp3s = []
         for root, directories, files in os.walk(dirname):
             for file in files:
-               if file[-3:] == '.mp3':
+                if file[-3:] == '.mp3':
                     mp3s.append({root, file})
         for path, hashname in mp3s:
-            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
+            os.rename(path + hashname, path + self.map[hashname])
         os.remove(restore_path)
-                
-     def generateName(self, seed=time()):
-          return hashlib.md5(str(seed)).hexdigest()
 
 
 def parse_arguments():
@@ -52,16 +54,17 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
+
 def main():
     args = parse_arguments()
-    Shuffler = shuffler()
+    shuffler = Shuffler()
     if args.subcommand == 'rename':
-          if args.output:
-                Shuffler.rename(args.dirname, 'restore.info')
-          else:
-                Shuffler.rename(args.dirname, args.output)
+        if args.output:
+            shuffler.rename(args.dirname, 'restore.info')
+        else:
+            shuffler.rename(args.dirname, args.output)
     elif args.subcommand == 'restore':
-        Shuffler.restore(args.dirname, args.restore_map)
+        shuffler.restore(args.dirname, args.restore_map)
     else:
         sys.exit()
 
