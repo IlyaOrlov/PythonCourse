17b905f4692320ebbf2153236b712f8bbaa912be
DenisRyzhov <54893681+DenisRyzhov@users.noreply.github.com>
2019-09-05 13:42:33 +0300

    Add files via upload
    The Initial  task file was commented accordingly PEP8 rules.

---
+++ Practice/pep8task_Ryzhov_1v1.py
@@ -0,0 +1,78 @@
+import sys
+import os
+import hashlib
+import ast
+import argparse
+from time import *
+
+
+class shuffler:  # ClassName with Capital letter
+
+    def __init__(self):
+        self.map = {}
+
+    def rename(self, dirname, output):
+          mp3s = []  # 4 spaces per indentation level
+        for root, directories, files in os.walk(dirname):
+            for file in files:
+                if file[-3:] == '.mp3':
+                    mp3s.append([root, file])
+        for path, mp3 in mp3s:
+            hashname = self.generateName() + '.mp3'
+            self.map[hashname] = mp3
+            os.rename(path + '/' + mp3), path + '/' + hashname))
+          f = open(output, 'r')  # Undefined indentation: is this line within 'for' or outside
+          f.write(str(self.map))  # same type misunderstanding
+
+    def restore(self, dirname, restore_path):
+          with open(filename, '+') as f:  # 4 spaces per indentation level
+            self.map = ast.literal_eval(f.read())  # 4 spaces per indentation \
+            # level, though there are 8 spaces from margin. \
+            # Correction of previous line will fix this one.
+          mp3s = []  # 4 spaces per indentation level
+        for root, directories, files in os.walk(dirname):
+            for file in files:
+               if file[-3:] == '.mp3':  # 4 spaces per indentation level
+                    mp3s.append({root, file})  # 4 spaces per indentation \
+                    # level, though there are 20 = 4*int spaces from margin. \
+                    # Correction of previous line will fix this one.
+        for path, hashname in mp3s:
+            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
+        os.remove(restore_path)
+                
+     def generateName(self, seed=time()):  # 4 spaces per indentation level
+          return hashlib.md5(str(seed)).hexdigest()
+
+
+def parse_arguments():  # There one type of PEP*_Error: \
+    # TLL = too long line (>79 letters)
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='subcommand', \  # TLL
+                                       help='subcommand help')
+    rename_parser = subparsers.add_parser('rename', help='rename help')
+    rename_parser.add_argument('dirname')
+    rename_parser.add_argument('-o', '--output', \  # TLL
+                         help='path to a file where restore map is stored')
+    # 'help' is not aligned with '(' on previous line to exclude TLL \
+    # due to Zen rule 9: 'Although practicality beats purity'
+    restore_parser = subparsers.add_parser('restore', help="command_a help")
+    restore_parser.add_argument('dirname')
+    restore_parser.add_argument('restore_map')
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_arguments()
+    Shuffler = shuffler()
+    if args.subcommand == 'rename':
+          if args.output:  # 4 spaces per indentation level
+                Shuffler.rename(args.dirname, 'restore.info')  # 4 spaces per indentation level
+          else:  # 4 spaces per indentation level (inheritance from line 66)
+                Shuffler.rename(args.dirname, args.output)  # 4 spaces per indentation level (inheritance from line 66)
+    elif args.subcommand == 'restore':
+        Shuffler.restore(args.dirname, args.restore_map)
+    else:
+        sys.exit()
+
+
+main()
