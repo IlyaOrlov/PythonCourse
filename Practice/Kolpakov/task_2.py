4ded612a686f3e8c1d750c046c1e06c9136e6676
yuriy <ykolpakov@mail.ru>
2019-09-11 16:13:07 +0300

    2 functions crete

---
+++ Practice/Kolpakov/task_2.py
@@ -0,0 +1,21 @@
+# №1
+def large_of_numbers_1(x, y):
+    if a > b:
+        print(a)
+    else:
+        print(b)
+
+
+# №2
+def large_of_numbers_2(x, y):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+
+a = int(input())
+b = int(input())
+large_of_numbers_1(a, b)
+print(large_of_numbers_2(a, b))
