32322d23d313cb70bd4c6d83c6755b2adad99581
Max Rodin <mister.jazzy12@yandex.ru>
2019-09-11 19:44:05 +0300

    Revert "Task2 первое и второе задание"
    This reverts commit 5ba1c8f5634e31fee72c37338040d3a12ff4fb75.

--- Practice/MaximRodin/Task2/Task2First.py
+++
@@ -1,8 +0,0 @@
-def taskone():
-    a=input()
-    b=input()
-    if a>b:
-        print(a)
-    elif b>a:
-        print(b)
-taskone()
--- Practice/MaximRodin/Task2/Task2Second.py
+++
@@ -1,6 +0,0 @@
-def task(a,b):
-    return max(a,b)
-
-a=input()
-b=input()
-print('Максимальное значение:', task(a,b))
