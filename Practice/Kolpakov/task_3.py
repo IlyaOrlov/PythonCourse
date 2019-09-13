0c27bca756a8ceba48a89a31f9399053cde90f83
yuriy <ykolpakov@mail.ru>
2019-09-13 15:17:53 +0300

    task_3

---
+++ Practice/Kolpakov/task_3.py
@@ -0,0 +1,22 @@
+# NUM 1
+"""for i in range(1, 101):
+    #print(i, end = ',')
+    if i % 15 == 0:
+        i = 'FizzBuzz'
+    elif i % 3 == 0:
+        i = 'Fizz'
+    elif i % 5 == 0:
+        i = 'Buzz'
+    print(i)"""
+
+
+# NUM 2
+x = input()
+if len(x) == 5:
+    for i in x:
+        print('cifra = ', i)
+else:
+    print('Число не 5-ти значное')
+
+
+#NUM 3
