0a052a8811e53b665cb2eff86b78d1cf455c80f4
DenisRyzhov <denis.a.ryzhov@gmail.com>
2019-09-16 17:00:21 +0300

    variable names are changed and the transposition in sort method is improved

--- Practice/RyzhovD/RyzhovDenis_Lec4_task6_del_column.py
+++ Practice/RyzhovD/RyzhovDenis_Lec4_task6_del_column.py
@@ -5,63 +5,66 @@ For tutorial purposes matrix is generated
 as an array of random integer numbers.
 """
 
-from random import randint as Random_integer
+from random import randint as random_integer
 # random.randint(A, B) generates a random integer number N, A ? N ? B.
 
-Ny = int(input('Please, enter number of rows: '))
-Nx = int(input('Please, enter number of columns: '))
+ny = int(input('Please, enter number of rows: '))
+nx = int(input('Please, enter number of columns: '))
 
-Rand_min = int(input('Let us define the range of your random numbers. '
+rand_min = int(input('Let us define the range of your random numbers. '
                      'Enter minimual possible number: '))
-Rand_max = int(input('Enter maximual possible number: '))
+rand_max = int(input('Enter maximual possible number: '))
 
 # Generation of random matrix
-A = []
-for i in range(0,Ny):
-    Row = []
-    for j in range(0,Nx):
-        Element_ij = Random_integer(Rand_min, Rand_max)
-        Row.append(Element_ij)
-    A.append(Row)
+a = []
+for i in range(0,ny):
+    row = []
+    for j in range(0,nx):
+        element_ij = random_integer(rand_min, rand_max)
+        row.append(element_ij)
+    a.append(row)
 
-if Nx and Ny < 7:
-    print(str(A))  # Print matrix.
+if nx and ny < 7:
+    print(str(a))  # Print matrix.
 
-Fate = int(input('Enter the specific integer number '
+fate = int(input('Enter the specific integer number '
                  'and I delete all rows that contain it. Your choice: '))
 
 # Identification 'fatal' rows which contain the specific number.
-Fatal_rows_indices = []
-for i in range(0,Ny):
-    Row_susp = A[i]
-    print(str(Row_susp))
-    for j in range(0,Nx):
-        if Row_susp[j] == Fate:
-            Fatal_rows_indices.append(j)
+fatal_columns_indices = []
+for i in range(0,ny):
+    row_susp = a[i]
+    print(str(row_susp))
+    for j in range(0,nx):
+        if row_susp[j] == fate:
+            fatal_columns_indices.append(j)
 
-# print('Fatal Indices mess = ' + str(Fatal_rows_indices))  ## D&C
-Fatal_rows_indices = set(Fatal_rows_indices)
-Fatal_rows_indices = list(Fatal_rows_indices)
-#print('Fatal Indices unique = ' + str(Fatal_rows_indices))  ## D&C
+# print('Fatal Indices mess = ' + str(fatal_columns_indices))  ## D&C
+fatal_columns_indices = set(fatal_columns_indices)
+fatal_columns_indices = list(fatal_columns_indices)
+#print('Fatal Indices unique = ' + str(fatal_columns_indices))  ## D&C
 
 # Here we sort 'fatal' indices in (as in Task 3 for Lecture 4)
-s = Fatal_rows_indices
+s = fatal_columns_indices
 for k in range(0,len(s)):
     for i in range(k,len(s)-1):
         if s[i+1] < s[i]:
-            a = s[i:i+2]
-            s[i:i+2] = a[::-1]
+            s[i], s[i + 1] = s[i + 1], s[i]  # Here I realize
+            # the superiority of this method:
+            # it does not need to declare new variable
+            # and thus the possible conflict of variable names is avoided.
 
-Fatal_rows_indices = s[::-1]
+# Invert the order of indices to delete from the far end.
+fatal_columns_indices = s[::-1]
 # print('Fatal Indices sorted = ' + str(Fatal_rows_indices))  ## D&C
 
 # Delete a column in matrix if this column contains the chosen number
-A_slim =[]
-for i in range(0,Ny):
-    Row = A[i]
-    for k in Fatal_rows_indices:
-        del Row[k]
-    A_slim.append(Row)
+a_slim =[]
+for i in range(0,ny):
+    row = a[i]
+    for k in fatal_columns_indices:
+        del row[k]
+    a_slim.append(row)
 
-for i in range(0,Ny):
-    print(str(A_slim[i]))
+for i in range(0,ny):
+    print(str(a_slim[i]))
