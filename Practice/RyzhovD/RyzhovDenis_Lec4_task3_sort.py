c69a3c3f3fa89accef6ddb159bc44f6b2badeda5
DenisRyzhov <denis.a.ryzhov@gmail.com>
2019-09-16 14:13:24 +0300

    This module sorts numbers in the list.

---
+++ Practice/RyzhovD/RyzhovDenis_Lec4_task3_sort.py
@@ -0,0 +1,15 @@
+"""
+This module sorts numbers in the list.
+"""
+
+s = [0, 3, 0, 9, 2, 0, 1, 9]
+# s = [4, 8, 5, 9, 5, 8, 6, 8, 84654, 84, 58, 84]
+
+print(str(s))
+for k in range(0,len(s)):
+    for i in range(k,len(s)-1):
+        if s[i+1] < s[i]:
+            a = s[i:i+2]
+            s[i:i+2] = a[::-1]
+
+print(str(s))
