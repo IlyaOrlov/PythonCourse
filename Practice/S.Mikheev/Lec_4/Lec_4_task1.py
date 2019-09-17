1eb0216293742a7bdfe74aeff2e7104d51d89c20
Sergey <muxee4ka@gmail.com>
2019-09-17 18:19:00 +0300

    Поправил по замечаниям задания1 и 5 к 4 лекции

--- Practice/S.Mikheev/Lec_4/Lec_4_task1.py
+++ Practice/S.Mikheev/Lec_4/Lec_4_task1.py
@@ -1,9 +1,9 @@
 for i in range(1, 101):
-    if i % 3 != 0 and i%5 != 0:
-        print(i, end=" ")
+    if i % 15 == 0:
+        print("FizzBuzz", end=" ")
     elif i % 3 == 0 and i%5 != 0:
         print("Fizz", end=" ")
     elif i % 3 != 0 and i % 5 == 0:
         print("Buzz", end=" ")
     else:
-        print("FizzBuzz", end=" ")
+        print(i, end=" ")
--- Practice/S.Mikheev/Lec_4/Lec_4_task5.py
+++ Practice/S.Mikheev/Lec_4/Lec_4_task5.py
@@ -3,12 +3,10 @@ d = {'test': 'qwerty', 'python': 'abcd', 'new': 'old'}
 
 def interstring(str):
     str = str.lower()
-    lst = str.split()
-    for elem in lst:
-        if elem in d:
-            str = str.replace(elem, d[elem])
+    for elem in d:
+        str = str.replace(elem, d[elem])
     return str
 
 
-print(interstring('This is my first test on a python. '
+print(interstring('This is my first testing on a python. '
                   'Python is a new programming language for me and this test for experience.'))
