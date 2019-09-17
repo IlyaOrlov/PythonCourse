646e0a4e051a1ac15e56134f439ccb56ec3bcc4a
DenisRyzhov <denis.a.ryzhov@gmail.com>
2019-09-17 14:28:56 +0300

    Here I just use permutation as s[i], s[i + 1] = s[i + 1], s[i] , since it more python-like and do not use additional variable

--- Practice/RyzhovD/RyzhovDenis_Lec4_task3_sort.py
+++ Practice/RyzhovD/RyzhovDenis_Lec4_task3_sort.py
@@ -1,5 +1,5 @@
 """
-This module sorts numbers in the list.
+This module sorts numbers in the list (bubble sort).
 """
 
 s = [0, 3, 0, 9, 2, 0, 1, 9]
@@ -9,7 +9,9 @@ print(str(s))
 for k in range(0,len(s)):
     for i in range(k,len(s)-1):
         if s[i+1] < s[i]:
-            a = s[i:i+2]
-            s[i:i+2] = a[::-1]
+            # a = s[i:i+2]
+            # s[i:i+2] = a[::-1]
+            # simplest substitution
+            s[i], s[i + 1] = s[i + 1], s[i]
 
 print(str(s))
