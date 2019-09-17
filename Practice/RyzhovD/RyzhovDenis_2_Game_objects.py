d8b11c3dbb3c40ee87606c01cf9cc21b90d644ca
DenisRyzhov <denis.a.ryzhov@gmail.com>
2019-09-17 15:07:14 +0300

    Additional actions are added. For class Reptile method move now depends on speed and artifact.

--- Practice/RyzhovD/RyzhovDenis_2_Game_objects.py
+++ Practice/RyzhovD/RyzhovDenis_2_Game_objects.py
@@ -2,54 +2,64 @@
 Superclass and subclasses are created.
 Different types of movement are defined as printable strings (for simplicity) \
 as a function of speed.
-Definitions of different types of shooting etc. are supposed further.
-Encapsulation ( with extra '_' ?) is not done, since is not understood yet.
+
+Additional actions are added.
+For class Reptile method move now depends on speed and artifact.
 """
 
 class Master:  #
-    def __init__(self, name, shoot, pick):
+    def __init__(self, name):
         # attribute definition
         self.name = name
-        self.shoot = shoot
-        self.pick = pick
 
     def move(self, speed):  # move on the ground
-        print(self.name + ' runs with the speed of ' + str(speed))
+        print(self.name + ' runs with the speed of ' + str(speed) + '.')
 
-po = Master('Po', 'shoot', 'pick')
-shifu = Master('Shifu', 'shoot', 'pick')
-tigress = Master('Tigress', 'shoot', 'pick')
-monkey = Master('Monkey', 'shoot', 'pick')
+    def act(self, mark):
+        if mark == 'p':
+            print(self.name + ' picks smth.')
+        elif mark == 's':
+            print(self.name + ' shoots from smth.')
+
+
+po = Master('Po')
+shifu = Master('Shifu')
+tigress = Master('Tigress')
+monkey = Master('Monkey')
 
 class Snake(Master):
     def move(self, speed):  # move on the ground
-        print(self.name + ' creeps with the speed of ' + str(speed))
+        print(self.name + ' creeps with the speed of ' + str(speed) + '.')
         # creep = to move slowly with the abdomen close to the ground
 
-viper = Snake('Viper', 'shoot', 'pick')
+viper = Snake('Viper')
 
 
 class Reptile(Master):
-    def move(self, speed):  # move on the ground
-        print(self.name + ' crawls with the speed of ' + str(speed))
+    def move(self, speed, artef):  # move on the ground
+        if artef == 'magic_pants':
+            print(self.name + ' flies with the speed of ' + str(10*speed) + '.')
+            # Ability to fly with 10x speed if your reptile wears magic pants
+        else:
+            print(self.name + ' crawls with the speed of ' + str(speed) + '.')
         # crawl = to move slowly on hands and knees, \
         # or by dragging the body along the ground
 
-oogway = Master('Oogway', 'shoot', 'pick')
+oogway = Reptile('Oogway')
 
 
 class Insect(Master):
     def move(self, speed):  # move on the ground
-        print(self.name + ' jumps with the speed of ' + str(speed))
+        print(self.name + ' jumps with the speed of ' + str(speed) + '.')
 
-mantis = Master('Mantis', 'shoot', 'pick')
+mantis = Insect('Mantis')
 
 
 class Bird(Master):
     def move(self, speed):  # move on the ground
-        print(self.name + ' flies with the speed of ' + str(speed))
+        print(self.name + ' flies with the speed of ' + str(speed) + '.')
 
-crane = Master('Mantis', 'shoot', 'pick')
+crane = Bird('Crane')
 
 
 ### TIME TO TEST
@@ -58,5 +68,30 @@ shifu.move(3)
 tigress.move(6)
 monkey.move(4)
 viper.move(2)
-oogway.move(0.1)
+oogway.move(0.1, 0)
 mantis.move(5)
+crane.move(3.3)
+
+### ANOTHER TEST
+
+hero_list = [po, shifu, tigress, monkey, viper, oogway, mantis, crane]
+hero_speed = [100, 300, 600, 400, 200, 10, 500, 330]
+hero_act = ['p', 's', 'p', 's', 'p', 's', 'p', 's']
+
+some_speed = 5
+
+print('')
+for h in hero_list:
+    if h == oogway:
+        h.move(some_speed,'magic pants')
+    else:
+        h.move(some_speed)
+
+print('')
+for k in range(0, len(hero_list)):
+    if h == oogway:
+        h.move(some_speed,'magic pants')
+    else:
+        h.move(some_speed)
+    print('   and')
+    hero_list[k].act(hero_act[k])
