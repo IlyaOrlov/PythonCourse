d5b198deb27111c519e1d6f30651ec2f5b2930d4
Sergey <muxee4ka@gmail.com>
2019-09-18 18:46:48 +0300

    Поправил задания 1 и 6 к лекции 4

--- Practice/S.Mikheev/Lec_4/Lec_4_task1.py
+++ Practice/S.Mikheev/Lec_4/Lec_4_task1.py
@@ -1,9 +1,9 @@
 for i in range(1, 101):
     if i % 15 == 0:
         print("FizzBuzz", end=" ")
-    elif i % 3 == 0 and i%5 != 0:
+    elif i % 3 == 0:
         print("Fizz", end=" ")
-    elif i % 3 != 0 and i % 5 == 0:
+    elif i % 5 == 0:
         print("Buzz", end=" ")
     else:
         print(i, end=" ")
--- Practice/S.Mikheev/Lec_4/Lec_4_task6.py
+++ Practice/S.Mikheev/Lec_4/Lec_4_task6.py
@@ -1,11 +1,16 @@
 n = int(input())  # количество строк матрицы
 a = [[int(j) for j in input().split()] for i in range(n)]  # создаем матрицу из n строк
 m = int(input())  # цифра, столбец с которой надо удалить
+k = []  # список индексом строк, в которых содержится цифра 'm'
 for i in reversed(range(n)):
     for j in reversed(range(len(a[i]))):
         if a[i][j] == m:
-            for i in range(n):
-                del (a[i][j])
+            if j in k:
+                k.append(j)
+k = sorted(k)
+for elem in reversed(k):
+    for i in reversed(range(len(a))):
+        del (a[i][elem])
 
 for row in a:
     print(*row)
