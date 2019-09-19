688b0bf5926cf67fa6a345af0e4acd082537f5ea
Sergei Mikheev <54879069+Muxee4ka@users.noreply.github.com>
2019-09-19 15:05:49 +0300

    Update Lec_4_task6.py

--- Practice/S.Mikheev/Lec_4/Lec_4_task6.py
+++ Practice/S.Mikheev/Lec_4/Lec_4_task6.py
@@ -2,14 +2,14 @@ n = int(input())  # количество строк матрицы
 a = [[int(j) for j in input().split()] for i in range(n)]  # создаем матрицу из n строк
 m = int(input())  # цифра, столбец с которой надо удалить
 k = []  # список индексом строк, в которых содержится цифра 'm'
-for i in reversed(range(n)):
-    for j in reversed(range(len(a[i]))):
+for i in range(n):
+    for j in range(len(a[i])):
         if a[i][j] == m:
-            if j in k:
+            if j not in k:
                 k.append(j)
 k = sorted(k)
 for elem in reversed(k):
-    for i in reversed(range(len(a))):
+    for i in range(len(a)):
         del (a[i][elem])
 
 for row in a:
