4aab0c9b9c8dea279d7b958830a50d67a5d9fc90
leksiam <39030556+leksiam@users.noreply.github.com>
2019-09-19 15:43:15 +0300

    Задача 5.1

---
+++ Practice/A.Babintsev/Task_5/task5_1.py
@@ -0,0 +1,16 @@
+"""
+Написать класс Man, который принимает имя в конструкторе. Имеет
+метод solve_task, который просто выводит "I'm not ready yet".
+"""
+
+
+class Man:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.login = (first_name[0] + last_name).lower()
+    def solve_task():
+        print("I'm not ready yet")
+        
+Man.solve_task()
+  
