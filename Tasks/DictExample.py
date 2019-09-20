58f94d7cb29fe208d29293a39c0a98ebd5bf5f37
DenisRyzhov <denis.a.ryzhov@gmail.com>
2019-09-20 14:11:27 +0300

    1. The pair with key stop is also generated. To avoid this, may be this if-condition should be after input? 2. Why for we check input key in the set of values?

--- Tasks/DictExample.py
+++ Tasks/DictExample.py
@@ -1,11 +1,15 @@
 d = {}
 while True:
-   a = input()
-   if a not in d:
+   a = input('Input key :')  # String just to indicate the 'need to input'
+   if a == "stop": # The pair with key 'stop' is also generated.
+       # To avoid this, may be this if-condition should be after input?
+       break
+   if a not in d:  # Why for we check input key in the set of values?
        d[a] = 1
    else:
        d[a] += 1
-   if a == "stop":
+   if a == "stop": # The pair with key 'stop' is also generated/
+       # To avoid this, may be this if-condition should be after input?
        break
 
 for i in d.items():
