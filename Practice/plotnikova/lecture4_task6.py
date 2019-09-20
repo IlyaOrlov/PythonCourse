d031da0b3f2a5a121760a4cc7e7ff6bde3d5cd28
Плотникова Ольга Владимировна <o.plotnikova@etpgpb.local>
2019-09-20 12:34:58 +0300

    4_6 edit1

--- Practice/plotnikova/lecture4_task6.py
+++ Practice/plotnikova/lecture4_task6.py
@@ -1,17 +1,16 @@
 # транспонирование матрицы
 def transpose_matrix(a):
-    return [[a[j][i] for j in range(len(a))] for i in range(len(a[0]))]
+    if len(a)!=0:
+        return [[a[j][i] for j in range(len(a))] for i in range(len(a[0]))]
+    else:
+        return a
 
 
 # Удаление столбцов
 def delete_column(a, b):
     for row in a:
-        try:
-            row.index(b)
-        except ValueError:
-            pass
-        else:
-            a.pop(a.index(row))
+        if b in row:
+            a.remove(row)
     return a
 
 
@@ -22,7 +21,9 @@ matrix = [[1, 2, 3, 8, 3],
           [4, 5, 6, 35, 5],
           [4, 5, 6, 44, 2]]
 
+#matrix=[]
+
 # Число для удаления столбца
-b = 5
+b =5
 #ответ:
 print(transpose_matrix(delete_column(transpose_matrix(matrix), b)))
