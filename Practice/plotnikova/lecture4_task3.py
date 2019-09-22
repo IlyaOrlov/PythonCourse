6d30fc681c2944f412a8dd3b37f6258504ac27e3
povpov <5865163+povpov@users.noreply.github.com>
2019-09-22 10:06:32 +0300

    4_2,3

---
+++ Practice/plotnikova/lecture4_task3.py
@@ -0,0 +1,21 @@
+#найти наименьший элемент в массиве
+def find_min_arr(data,ii):
+    min_arr = data[ii]
+    for i in range(ii, len(data)):
+       if min_arr>data[i]:
+           min_arr=data[i]
+    return min_arr
+
+
+def sort(data):
+    t=len(data)
+    for i in range(t):
+        min_arr_index= data.index(find_min_arr(data, i))
+        data[i], data[min_arr_index] = data[min_arr_index], data[i]
+    return data
+
+
+arr=[-12, 33, 155, 0,3,24,2,-103,7, -14, 15]
+print(sort(arr))
+
+
