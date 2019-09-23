1a78114109cce0ab5c080ced98ee04877d290f6b
Max Rodin <mister.jazzy12@yandex.ru>
2019-09-23 19:11:48 +0300

    Добавил отступы

--- Practice/MaximRodin/Task5/First.py
+++ Practice/MaximRodin/Task5/First.py
@@ -3,8 +3,11 @@ class Man:
         self.name = name
         self.surname = surname
         self.login = (name + surname)
+
     def solve_task(self):
         print("I am not ready yet")
+
+
 Worker = Man("Max", "Rodin")
 print(Worker.login)
 Worker.solve_task()
