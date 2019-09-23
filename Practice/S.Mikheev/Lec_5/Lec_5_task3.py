29901f328279d62ef22f08efff7f79847699fd25
Sergei Mikheev <54879069+Muxee4ka@users.noreply.github.com>
2019-09-23 13:52:14 +0300

    Update Lec_5_task3.py
    Поправил по замечаниям.

--- Practice/S.Mikheev/Lec_5/Lec_5_task3.py
+++ Practice/S.Mikheev/Lec_5/Lec_5_task3.py
@@ -1,24 +1,34 @@
 import os
+import tempfile
 
 
 class WrapStrToFile:
     def __init__(self):
-        self.filepatch = 'test.txt'
+        self.filepatch = tempfile.mktemp()
 
     @property
     def content(self):
         try:
             f = open(self.filepatch, 'r')
-        except Exception as exc:  # Можно написать: except FileNotFoundError:
-            return "File doesn't exist. Exception name: {}".format(type(exc).__name__)
+        except  FileNotFoundError:  # Ошибка если файл не найден
+            return "File doesn't exist."
+        except Exception as exc:
+            return "Some unexpected error: {}".format(type(exc).__name__)
         else:
             return f.read()
 
     @content.setter
     def content(self, value):
-        f = open(self.filepatch, 'w')
-        f.write(value)
-        f.close()
+        try:
+            f = open(self.filepatch, 'w')
+        except PermissionError:  # Ошибка недостаточно прав для записи
+            return "Editing is not available."
+        except Exception as exc:
+            return "Some unexpected error: {}".format(type(exc).__name__)
+        else:
+            f.write(value)
+        finally:
+            f.close()
 
     @content.deleter
     def content(self):
