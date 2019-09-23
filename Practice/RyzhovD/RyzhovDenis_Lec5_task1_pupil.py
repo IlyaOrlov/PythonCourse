10e81e1cb682896cd7c3c5d1d388fb9109bdeafd
DenisRyzhov <denis.a.ryzhov@gmail.com>
2019-09-23 16:51:55 +0300

    Minor improvements: This file create the class Man, which instance is always not ready yet, and subclass Pupil where think from 3 to 6 second. Changes: definitions of class instances with brackets and addition name as an attribute (It looks better, in my opinion). It allows me to practise with the construction {} ..format(), that is really more convenient than the concatenation with +.

---
+++ Practice/RyzhovD/RyzhovDenis_Lec5_task1_pupil.py
@@ -0,0 +1,35 @@
+"""
+This file create the class Man, which instance is always not ready yet,
+and subclass Pupil where think from 3 to 6 second.
+Changes: definitions of class instances with brackets
+and addition 'name' as an attribute (It looks better, in my opinion).
+It allows me to practise with the construction ' {} ..format('') ',
+that is really more convenient than the concatenation with '+'.
+"""
+
+from random import randint
+from time import sleep
+
+class Man:
+    def __init__(self, name):
+        self.name = name
+
+    def solve_task(self, name):
+        print('{} says: I\'m not ready yet.'.format(name))
+
+captain = Man('Captain')
+captain.solve_task(captain.name)
+print('')
+
+
+class Pupil(Man):
+
+    def solve_task(self, name):
+        tau = randint(3, 6)
+        print('{} starts to think...'.format(name))
+        sleep(tau)
+        print('And now he says: I\'ve thought for ' + str(tau) + ' seconds.'
+                                                ' And I don\'t know.')
+
+rookie = Pupil('Rookie')
+rookie.solve_task(rookie.name)
