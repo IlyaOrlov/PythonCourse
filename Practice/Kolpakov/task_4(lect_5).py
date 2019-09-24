2b5384329b223ebfa8c2bdbf65e1ca51b569dda4
yuriy <ykolpakov@mail.ru>
2019-09-24 13:46:48 +0300

    task_4, лекция №5 добавлено задание №3

--- Practice/Kolpakov/task_4(lect_5).py
+++ Practice/Kolpakov/task_4(lect_5).py
@@ -25,3 +25,48 @@ class Pupil(Man):
 
 b = Pupil('Mark', 'Tven')
 print(b.solve_task())
+
+
+#Num 3
+import tempfile
+import os
+
+class WrapStrToFile:
+    def __init__(self):
+        self.filepath = tempfile.mktemp()
+
+# здесь инициализируется атрибут filepath,
+# он содержит путь до файла-хранилища
+    @property
+    def content(self):
+        try:
+            fo = open(self.filepath, 'r')
+        except FileNotFoundError:
+            return "File doesn't exist"
+        except Exception as exc:
+            return "Some unexpected error: {}".format(exc)
+        else:
+            return fo.read()
+
+# попытка чтения из файла, в случае успеха возвращаем содержимое
+# в случае неудачи возвращаем 'File doesn't exist'
+    @content.setter
+    def content(self, value):
+        fo = open(self.filepath, 'w')
+        fo.write(value)
+        fo.close()
+
+# попытка записи в файл указанного содержимого
+    @content.deleter
+    def content(self):
+        os.remove(self.filepath)
+
+
+wstf = WrapStrToFile()
+print(wstf.content)  # Output: File doesn't exist
+wstf.content = 'test str'
+print(wstf.content)  # Output: test_str
+wstf.content = 'text 2'
+print(wstf.content)  # Output: text 2
+del wstf.content
+# после этого файла не существует
