6c4b234b7132af71d5e8d858f156920d36a05cfc
Sergey <muxee4ka@gmail.com>
2019-09-25 21:42:32 +0300

    Задания 3 и 4* к 6-ой лекции

---
+++ Practice/S.Mikheev/Lec_6/Lec_6_context_manager.py
@@ -0,0 +1,15 @@
+import time
+
+
+class MyRuntime:
+    def __enter__(self):
+        self.timestart = time.time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Runtime {} seconds'.format(time.time() - self.timestart))
+        if exc_val:
+            raise
+
+
+with MyRuntime():
+    print(1 + 1)
