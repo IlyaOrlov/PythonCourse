915dffadb3dd18adac93044e1aa8c9eb99c61184
pov <>
2019-09-25 15:02:26 +0300

    +5.1 edit

--- Practice/plotnikova/lecture5_task1.py
+++ Practice/plotnikova/lecture5_task1.py
@@ -1,11 +1,11 @@
 class Man():
 
-    def __init__(self, name=""):
+    def __init__(self, name="Вася"):
         self.name=name
         # self.solve_task()
 
     def solve_task(self):
-        return print ("I'm not ready yet")
+        return print ("{}: I'm not ready yet".format(self.name))
 
 
 #C self.solve_task() в конструторе
