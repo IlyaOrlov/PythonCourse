c71571a498afb814f313c5ed98866c1e033f3833
Sergei Mikheev <54879069+Muxee4ka@users.noreply.github.com>
2019-09-26 13:01:35 +0300

    Update Lec_6_context_manager.py
    Ошибку понял. Ловлю исключения убрал

--- Practice/S.Mikheev/Lec_6/Lec_6_context_manager.py
+++ Practice/S.Mikheev/Lec_6/Lec_6_context_manager.py
@@ -7,9 +7,7 @@ class MyRuntime:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print('Runtime {} seconds'.format(time.time() - self.timestart))
-        if exc_val:
-            raise
 
-
+        
 with MyRuntime():
     print(1 + 1)
