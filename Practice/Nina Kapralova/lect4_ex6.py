260294b701c1ddfdfe5aebc16f73be7fb9ec8bc0
nusyarik <nusyarik@mail.ru>
2019-09-27 23:46:58 +0300

    Correct tasks from lecture 4

--- Practice/Nina Kapralova/lect4_ex6.py
+++ Practice/Nina Kapralova/lect4_ex6.py
@@ -1,6 +1,10 @@
 def transpose(matr: list):
     res = []
     n = len(matr)
+    if n == 0:
+        print('Матрица {} пустая. Невозможно удалить '
+              'столбец пустой матрицы!'.format(matr))
+        raise SystemExit
     m = len(matr[0])
     for j in range(m):
         tmp = []
@@ -22,6 +26,9 @@ a = [[1, 2, 3],
      [4, 5, 6],
      [7, 8, 9]
      ]
+# пустая матрица для проверки
+b = []
 
 
 print(transpose(del_column(transpose(a), 5)))  # пример проверки функции
+print(transpose(del_column(transpose(b), 5)))  # пример проверки функции для пустой матрицы
