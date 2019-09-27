462d0e987c322553c77ec44c404630875461ff7a
pov <>
2019-09-27 11:04:22 +0300

    6_3 edit

--- Practice/plotnikova/lecture6_task3.py
+++ Practice/plotnikova/lecture6_task3.py
@@ -7,13 +7,11 @@ class ContextManager():
 
     def __enter__(self):
         print('Starting code inside')
-        for i in range(10000): # для нагрузки, чтобы на выходе был не 0
-            a = i**2
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print("--- %s seconds ---" % (time.time() - self.start_time))
 
 
 with ContextManager():
-    pass
-
+    for i in range(10000):  # для нагрузки, чтобы на выходе был не 0
+        a = i ** 2
