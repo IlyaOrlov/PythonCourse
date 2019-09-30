eb23b219584520f7f96c413724ec388be49da30d
povpov <5865163+povpov@users.noreply.github.com>
2019-10-01 00:00:26 +0300

    +8_1-8_6

---
+++ Practice/plotnikova/lecture8_task6.py
@@ -0,0 +1,32 @@
+# Написать реализацию функции format
+
+
+def my_format(* args):
+    res = args[0]
+    count=res.count("{}")
+    for idx, val in enumerate(args):
+        if idx!=0:
+            if count==0:
+                if res.count("{" + str(idx - 1) + "}") > 0:
+                    res = res.replace("{" + str(idx - 1) + "}", val)
+                else:
+                    return "Ошибка аргументов1"
+            else:
+                if res.count("{}") > 0:
+                    res = res.replace("{}", val, 1)
+                else:
+                    return "Ошибка аргументов2"
+    return res
+
+
+a=my_format("coordinates: {} и {}", '33','44')
+b=my_format("coordinates: {100} и {0}", '33','44')
+c=my_format("coordinates: {1} и {0}", '33','44')
+d=my_format("coordinates: {0} и {}", '33','44')
+f=my_format("coordinates: {0} и {2}", '33','44')
+
+print("a = {}". format(a))
+print("b = {}". format(b))
+print("c = {}". format(c))
+print("d = {}". format(d))
+print("f = {}". format(f))
