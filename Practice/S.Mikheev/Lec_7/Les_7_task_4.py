05c7bcfec45059c23b69ffa1c43c6299f29bcf39
Sergey <muxee4ka@gmail.com>
2019-10-03 09:41:34 +0300

    Заменил способ конкатенации на os.path.join

--- Practice/S.Mikheev/Lec_7/Les_7_task_4.py
+++ Practice/S.Mikheev/Lec_7/Les_7_task_4.py
@@ -7,13 +7,13 @@ def remover(path):
     while True:
         for current_dir, dirs, files in os.walk(path):
             for file in files:
-                delta = time.time() - os.path.getmtime(current_dir + '\\' + file)
+                delta = time.time() - os.path.getmtime(os.path.join(current_dir, file))
                 if delta > 60:
-                    os.remove(current_dir + '\\' + file)
+                    os.remove(os.path.join(current_dir, file))
             for direct in dirs:
-                delta2 = time.time() - os.path.getmtime(current_dir + '\\' + direct)
+                delta2 = time.time() - os.path.getmtime(os.path.join(current_dir, direct))
                 if delta2 > 120:
-                    shutil.rmtree(current_dir + '\\' + direct)
+                    shutil.rmtree(os.path.join(current_dir, direct))
 
 
 remover("test")
