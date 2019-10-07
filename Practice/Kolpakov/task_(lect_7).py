2db666298dfe2ca1dc13fde4f5a997aed1c74c25
yuriy <ykolpakov@mail.ru>
2019-10-07 19:42:17 +0300

    Задачи 1-3 лекция 7 (исправления 2)

--- Practice/Kolpakov/task_(lect_7).py
+++ Practice/Kolpakov/task_(lect_7).py
@@ -2,8 +2,7 @@ from datetime import date, timedelta
 from subprocess import Popen, PIPE
 
 
-
-#Num_1
+# Num_1
 # Написать функцию для подсчета количества рабочих дней между двумя
 # датами (даты передаются в качестве параметров).
 
@@ -25,29 +24,32 @@ print()
 print('---------------------------')
 
 
-#Num 2
-#С помощью библиотеки subprocess прочитать содержимое
-#произвольного файла с использованием утилиты cat в Linux или type в
-#Windows (имя файла должно передаваться как параметр в вашу
-#функцию).
+# Num 2
+# С помощью библиотеки subprocess прочитать содержимое
+# произвольного файла с использованием утилиты cat в Linux или type в
+# Windows (имя файла должно передаваться как параметр в вашу
+# функцию).
 
 def read(file_name):
     proc = Popen(['cat', file_name], stdout=PIPE, stderr=PIPE)
-    proc.wait() # дождаться выполнения
-    res = proc.communicate() # получить tuple('stdout', 'stderr')
+    proc.wait()  # дождаться выполнения
+    res = proc.communicate()  # получить tuple('stdout', 'stderr')
     if proc.returncode:
         return res[1]
     return 'result:', res[0]
+
 
 print(read('./file.txt'))
 print()
 print('---------------------------')
 
-#Num 3
+# Num 3
 # Создать класс Human с 5-10 атрибутами (имя, фамилия, возраст, меcто
 # жительства и т.д.)
 import random
 import pickle
+
+
 class Human:
     def __init__(self, name, age, location, profession, hobby):
         self.name = name
@@ -57,25 +59,33 @@ class Human:
         self.hobby = hobby
 
     def __repr__(self):
-        return f'{self.name}, {self.age} лет, страна рождения: {self.location}, профессия: {self.profession}, мое хобби: {self.hobby}\n'
+        return f'{self.name}, {self.age} лет, страна рождения: {self.location}, профессия: {self.profession}, мое ' \
+               f'хобби: {self.hobby}\n '
 
-name = ['Иннокентий', 'Фадей', 'Лада', 'Арсения', 'Владилен' , 'Аврор', 'Ипатия', 'Октябрина', 'Джозеф', 'Бенедикт']
-location = ['Россия', 'Беларусь', 'Греция', 'Словения', 'Мальдивы' , 'Куба', 'Вьетнам', 'Китай', 'Испания', 'Хорватия']
-profession = ['Хирург', 'Пилот', 'Исследователь', 'Космонавт', 'Продюсер', 'Биохимик', 'Частный детектив', 'Археолог', 'Дипломат', 'Кондитер', 'Корреспондент']
-hobby = ['Охота', 'Охота', 'Ролевые игры', 'Пейнтбол', 'Садоводство', 'Кулинария', 'Коллекционирование', 'Компьютерные игры', 'Чтение', 'фотография']
+
+name = ['Иннокентий', 'Фадей', 'Лада', 'Арсения', 'Владилен', 'Аврор', 'Ипатия', 'Октябрина', 'Джозеф', 'Бенедикт']
+location = ['Россия', 'Беларусь', 'Греция', 'Словения', 'Мальдивы', 'Куба', 'Вьетнам', 'Китай', 'Испания', 'Хорватия']
+profession = ['Хирург', 'Пилот', 'Исследователь', 'Космонавт', 'Продюсер', 'Биохимик', 'Частный детектив', 'Археолог',
+              'Дипломат', 'Кондитер', 'Корреспондент']
+hobby = ['Охота', 'Охота', 'Ролевые игры', 'Пейнтбол', 'Садоводство', 'Кулинария', 'Коллекционирование',
+         'Компьютерные игры', 'Чтение', 'фотография']
 
 
 def new_person(kol):
     humans = list()
     for i in range(kol):
-        humans.append(Human(random.choice(name), random.randint(20,65), random.choice(location), random.choice(profession), random.choice(hobby)))
-    yield humans
+        humans.append(
+            Human(random.choice(name), random.randint(20, 65), random.choice(location), random.choice(profession),
+                  random.choice(hobby)))
+
     with open('human.data', 'wb') as f:
         pickle.dump(humans, f)
+    return humans
+
 
 def read_new_person(file):
     with open(file, 'rb') as f:
-        yield pickle.load(f)
+        return pickle.load(f)
 
 
 for k in new_person(7):
