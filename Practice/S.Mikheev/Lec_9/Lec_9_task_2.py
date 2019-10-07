1ac42901cd17f84ed336ac11ac1071b3dd516102
Sergey <muxee4ka@gmail.com>
2019-10-07 18:56:34 +0300

    Поправил задание 2 из 9 лекции

--- Practice/S.Mikheev/Lec_9/Lec_9_task_2.py
+++ Practice/S.Mikheev/Lec_9/Lec_9_task_2.py
@@ -3,32 +3,32 @@ import multiprocessing
 
 
 def adder(*args):
-    flag = type(args[0])
-    a_num = 0
-    a_str = ''
+    if isinstance(args[0], list):
+        output = list()
+    elif isinstance(args[0], int) or isinstance(args[0], float):
+        output = 0
+    else:
+        output = ''
     start_time = time.time()
     print('Начало вычислений')
     for elem in args:
-        if flag is int or flag is float:
-            a_num += elem
-        else:
-            a_str += elem
-    if flag is int or flag is float:
-        print(a_num)
-    else:
-        print(a_str)
-    print('Конец операции сложения для типа {}, затрачено {} sec.'.format(flag, time.time() - start_time))
+        output += elem
+    print(output)
+    print('Конец операции сложения для типа {}, затрачено {} sec.'.format(type(args[0]), time.time() - start_time))
 
 
 if __name__ == '__main__':  # обязательно для многопроцессного приложения
     start_time = time.time()
+    multiprocess = []
     p1 = multiprocessing.Process(target=adder, args=('aaa', 'bbbb', 'cccc', 'dddd',))
+    multiprocess.append(p1)
     p2 = multiprocessing.Process(target=adder, args=(1, 2, 3, 4,))
+    multiprocess.append(p2)
     p3 = multiprocessing.Process(target=adder, args=(5.5, 7.8, 3.4, 5.6,))
-    p1.start()
-    p2.start()
-    p3.start()
-    p1.join()
-    p2.join()
-    p3.join()
+    multiprocess.append(p3)
+    p4 = multiprocessing.Process(target=adder, args=([1, 2, 3, 4, 5], [10, 55, 77, 89], ['asd', 'rrr'], [12.3, 13.2],))
+    multiprocess.append(p4)
+    for mp in multiprocess:
+        mp.start()
+        mp.join()
     print('Общее время вычислений в секундах: {}'.format(int(time.time() - start_time)))
