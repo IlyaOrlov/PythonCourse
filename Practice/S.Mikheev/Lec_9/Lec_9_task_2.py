acff6492df593f5f7ee2272a87181b68f538d9ac
Sergey <muxee4ka@gmail.com>
2019-10-08 16:58:33 +0300

    Update Lec_9_task_2 исправил функцию-генератор

--- Practice/S.Mikheev/Lec_9/Lec_9_task_2.py
+++ Practice/S.Mikheev/Lec_9/Lec_9_task_2.py
@@ -22,6 +22,8 @@ if __name__ == '__main__':  # обязательно для многопроце
             (1, 2, 3, 4,),
             (5.5, 7.8, 3.4, 5.6,),
             ([1, 2, 3, 4, 5], [10, 55, 77, 89], ['asd', 'rrr'], [12.3, 13.2],)]
+
+
     # processes = []
     # for arg in args:
     #     processes.append(Process(target=adder, args=arg))
@@ -30,14 +32,20 @@ if __name__ == '__main__':  # обязательно для многопроце
     # for p in processes:
     #     p.join()
 
-    def my_gen_proc(func, args):
+
+    def my_gen_proc(func, args):  # Функция-генератор процессов
         processes = []
         for arg in args:
             processes.append(Process(target=func, args=arg))
         for p in processes:
             p.start()
-        for p in processes:
-            yield p.join()
+            yield p
 
-    for _ in my_gen_proc(adder, args):
-        continue
+
+    plist = list(my_gen_proc(adder, args))  # список всех запущенных процессов
+
+    for p in plist:
+        p.join()
+
+    # for p in my_gen_proc(adder, args):
+    #     p.join()
