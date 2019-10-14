63887846f51957a15efed0b4dbc4c42938430d63
pov <>
2019-10-14 16:56:38 +0300

    9_7, 9_8 edit

--- Practice/plotnikova/lecture8_task7.py
+++ Practice/plotnikova/lecture8_task7.py
@@ -4,19 +4,21 @@ def copyfile(source, destination):
         with open(source, 'rt') as f:
            text= f.read()
     except FileNotFoundError:
-        return print ("Файл {} не найден".format(source))
+        return [-1, "Ошибка чтения файла {}.".format(source)]
     except Exception  as e:
-        return print("Ошибка чтения: {}".format(e))
+        return [-1, "Ошибка чтения файла {}.".format(source)]
+
     # Запись  файла
     try:
         with open(destination, 'x') as f:
             f.write(text)
     except FileExistsError:
-        return print ("Файл {} уже существует.".format(destination))
+        return [-1, "Ошибка записи файла {}.".format(destination)]
     except Exception  as e:
-        return print("Ошибка записи: {}".format(e))
-    return print("Файл {} успешно записан".format(destination) )
+        return [-2, "Ошибка записи файла {}.".format(destination)]
+    return [0, 'Файл {} скопирован успешно в {}'.format(source, destination)]
 
-source="1.txt"
-destination="2.txt"
-copyfile(source, destination)
+if __name__ == '__main__':
+    source="1.txt"
+    destination="2.txt"
+    print(copyfile(source, destination))
--- Practice/plotnikova/lecture8_task8.py
+++ Practice/plotnikova/lecture8_task8.py
@@ -1,24 +1,7 @@
 import os
 
+from Practice.plotnikova.lecture8_task7 import copyfile
 
-def copyfile(source, destination):
-    # Считывание файла
-    try:
-        with open(source, 'rt') as f:
-           text= f.read()
-    except FileNotFoundError:
-        return print ("Файл {} не найден".format(source))
-    except Exception  as e:
-        return print("Ошибка чтения: {}".format(e))
-    # Запись  файла
-    try:
-        with open(destination, 'x') as f:
-            f.write(text)
-    except FileExistsError:
-        return print ("Файл {} уже существует.".format(destination))
-    except Exception  as e:
-        return print("Ошибка записи: {}".format(e))
-    return print("Файл {} успешно записан".format(destination) )
 
 def copydir(old_dir, new_dir):
     if not os.path.isdir(old_dir):
