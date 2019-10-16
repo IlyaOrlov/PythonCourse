e0daede6bd554cca5e43b53b9eeb8818642aab0d
yuriy <ykolpakov@mail.ru>
2019-10-16 12:15:29 +0300

    Задачи 2 лекция 9 (исправления)

--- Practice/Kolpakov/lect_9/task_2.py
+++ Practice/Kolpakov/lect_9/task_2.py
@@ -7,32 +7,21 @@ lst_type = ['bfdQ', 5, 77, 'fgj'], [1, 2, '3Q'], [5, 6], ['468', 5, 'fgj']
 type_list = int_type, str_type, lst_type
 
 def addition(*args):
-    new_str_type = ''
-    new_int_type = 0
-    new_lst_type = []
     if type(args[0]) is str:
-        for i in args:
-            new_str_type += i
-        print(new_str_type)
+        new_type = ''
     elif type(args[0]) is int:
-        for i in args:
-            new_int_type += i
-        print(new_int_type)
+        new_type = 0
     elif type(args[0]) is list:
-        for i in args:
-            new_lst_type += i
-        print(new_lst_type)
+        new_type = []
+    for i in args:
+        new_type += i
+    print(new_type)
 
 
 if __name__ == '__main__':
     start = time.time()
-    num_start = 1
-    num_end = 0
     processes = []
     for j in type_list:
-        num_start += 1
-        num_end += 1
-        #print(j)
         p = Process(target=addition, args=j)
         p.start()
         processes.append(p)
