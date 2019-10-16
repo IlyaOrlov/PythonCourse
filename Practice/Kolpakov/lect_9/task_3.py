45fd51886bdc6b2b9822e272f206a5c88da870e4
yuriy <ykolpakov@mail.ru>
2019-10-16 12:32:03 +0300

    Задача 3 лекция 9 (исправления)

--- Practice/Kolpakov/lect_9/task_3.py
+++ Practice/Kolpakov/lect_9/task_3.py
@@ -3,11 +3,19 @@ import random
 
 
 def private_func():
+    try:
+        print(private_data)
+    except Exception:
+        print('No data')
     private_data = random.randint(0,100)
     print(f'Приватные данные: {private_data} {threading.current_thread().name}')
 
 
+threads = []
 for i in range(5):
     my_thread = threading.Thread(target=private_func)
     my_thread.start()
-    my_thread.join()
+    threads.append(my_thread)
+
+for j in threads:
+    j.join()
