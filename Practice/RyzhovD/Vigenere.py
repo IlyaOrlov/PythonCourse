74ff68bf71e2fbfe04c307ae527b4c5c9d6a81c6
DenisRyzhov <denis.a.ryzhov@gmail.com>
2019-10-17 12:48:56 +0300

    Only one function

--- Practice/RyzhovD/Vigenere.py
+++ Practice/RyzhovD/Vigenere.py
@@ -14,55 +14,36 @@ INPUT arguments are: string (text/data), key string, and an integer.
     Integer should be not multiple to 128 or equal to 0,
     otherwise text will not be encrypted.
 OUTPUT is a text (string).
+
+Realization with Python 3.6.
+Last date: 2019 Oct 16
+List of modifications:
+1. Realization with single for-loop
+    instead of previous version with 3 functions.
 """
 
-# Transform characters of text to the list of numbers
-# according ASCII table (128 symbols)
-def str_to_numlist(string):
-    numlst = []
-    for x in string:
-        numlst.append(int(ord(x)))
-    return numlst
-
-### variant with 3 functions
-# # Transform list of numbers to the text
-# # according ASCII table (128 symbols)
-# def numlst_to_str(numlst):
-#     string = ''
-#     for x in numlst:
-#         string = string + chr(x)
-#     return string
-
-### variant with 3 functions
-# # action may be coding (action=integer)
-# # or decoding (action=integer, which is opposite to coding action)
-# def Vigenere(text, coding_key_string, action):
-#     if action % 128 == 0:
-#         print('Please, choose an action in range [1 .. 127], '
-#               'otherwise your text will not be encrypted.')
-#         return None
-#     numlst_encr = []
-#     numlst_init = str_to_numlist(text)
-#     for k in range(len(numlst_init)):
-#         numlst_encr.append((numlst_init[k]
-#             + action*ord(coding_key_string[k % len(coding_key_string)])) % 128)
-#     text_encr = numlst_to_str(numlst_encr)
-#     return text_encr
-
-### variant with 2 functions
+# action may be coding (action=integer)
+# or decoding (action=integer, which is opposite to coding action)
 def Vigenere(text, coding_key_string, action):
     if action % 128 == 0:
         print('Please, choose an action in range [1 .. 127], '
               'otherwise your text will not be encrypted.')
         return None
-    numlst_init = str_to_numlist(text)
+    numlst_init = []
     text_encr = ''
-    for k in range(len(numlst_init)):
+    for k in range(len(text)):
+        # Transform characters of text to the list of numbers
+        # according ASCII table (128 symbols):
+        numlst_init.append(int(ord(text[k])))
+        # Displace symbols according to Vigenere mask:
         x = (numlst_init[k]
-            + action*ord(coding_key_string[k % len(coding_key_string)])) % 128
-    # text_encr = numlst_to_str(numlst_encr)
+             + action * ord(coding_key_string[k % len(coding_key_string)])) % 128
+        # Transform list of numbers to the text
+        # according ASCII table (128 symbols):
         text_encr += chr(x)
     return text_encr
+
+
 
 ### TEST
 string1 = 'Climb Mount Niitaka'
@@ -75,7 +56,3 @@ print('plaintext     : {}'.format(string1))
 string2 = Vigenere(string1, key_str, action)
 print('encrypted text: {}'.format(string2))
 print('decrypted text: {}'.format(Vigenere(string2, key_str, -action)))
-print('---')
-print(str_to_numlist(string1))
-print(str_to_numlist(string2))
-print(str_to_numlist(Vigenere(string2, key_str, -action)))
