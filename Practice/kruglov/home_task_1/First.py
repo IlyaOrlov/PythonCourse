a4c14afcaf8c9487d747e550561409a8cd4764a3
Darshniderd <darshn@mail.ru>
2019-12-09 22:19:34 +0300

    First two homework assignments from PyLec_2

---
+++ Practice/kruglov/home_task_1/First.py
@@ -0,0 +1,18 @@
+# 1. Function returns the maximum number of two variables
+def max_out(a, b):
+    if a > b:
+        print(a)
+    else:
+        print(b)
+
+
+# 2. The function displays the maximum number of two variables
+def maximum_num(x, y):
+    if x > y:
+        return x
+    else:
+        return y
+
+
+max_out(17, 19)
+print(maximum_num(8, 13))
