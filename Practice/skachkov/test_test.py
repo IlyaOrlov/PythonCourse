db4ff19984b232c0e7e693ba2e2b66421d4f45d1
SkachkovPetr <58567557+SkachkovPetr@users.noreply.github.com>
2019-12-11 19:54:57 +0300

    My practice_2 file

---
+++ Practice/skachkov/test_test.py
@@ -0,0 +1,33 @@
+def compare_nums():
+    number_1 = int(input("Введите число #1:"))
+    number_2 = int(input("Введите число #2:"))
+    if number_1 > number_2:
+        print(number_2, "Число меньше")
+    elif number_1 == number_2:
+        print("Число равно")
+    else:
+        print(number_2,"Число больше")
+
+
+# print(number_1)
+# print(number_2)
+compare_nums()
+
+
+ # class pizza()
+
+# def PizzaMaker(p)
+# p.make Base()
+# if type(p) == ///
+#     p.add Meat()
+# elif
+#
+# p.addIng()
+# super()# явное обращение к классу родителя
+# def change status
+# status = newStatus
+# changeStatus(ingred add)
+
+
+
+
