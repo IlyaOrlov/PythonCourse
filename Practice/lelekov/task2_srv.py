f8671ff28760c516e5007fd27a6d7ccba3c3b2a7
SergeyLelekov <sslelekov@gmail.com>
2019-12-11 15:35:02 +0300

    добавлен файл с заданием 2 - функции сравнения

---
+++ Practice/lelekov/task2_srv.py
@@ -0,0 +1,30 @@
+def sravni_prnt(a, b):
+    if a == b:
+        print('Числа равны')
+    elif a > b:
+        print (a)
+    else:
+        print (b)
+
+def sravni_rtrn(a, b):
+    if a == b:
+        print('Числа равны')
+    elif a > b:
+        return a
+    else:
+        return b
+
+
+x = int(input('Введите число 1 ',))
+y = int(input('Введите число 2 ',))
+print('Введите номер функции:')
+print('1- Функция, принимающую два числа и выводящую на экран большее из двух')
+print('2- Функция, принимающую два числа и возвращающую большее из двух')
+vib = int(input('Введите номер ',))
+if vib == 2:
+   z = sravni_rtrn(x, y)
+   print (z)
+elif vib == 1:
+     sravni_prnt(x, y)
+else:
+    print('Выбор не верен :)')
