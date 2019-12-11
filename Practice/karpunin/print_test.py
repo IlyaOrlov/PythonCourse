ff5e94e0889badd86a2d9f94a988fcc3819c7686
AndreyKarpunin <58586908+AndreyKarpunin@users.noreply.github.com>
2019-12-11 16:16:27 +0300

    Добавил файлы из задания 2

---
+++ Practice/karpunin/print_test.py
@@ -0,0 +1,13 @@
+def sravnit(a, b):
+    if a > b:
+        print(a)
+    elif a == b:
+        print ("равно")
+    else:
+        print(b)
+a = float(input())
+b = float(input())
+sravnit(a, b)
+
+
+
---
+++ Practice/karpunin/return_test.py
@@ -0,0 +1,8 @@
+def sravnit(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+a = float(input())
+b = float(input())
+c = sravnit(a, b)
