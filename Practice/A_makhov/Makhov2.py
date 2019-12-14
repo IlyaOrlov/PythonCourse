5a174ed0ef6084163e1aa332d6cd9247d2c4d421
Dron239 <Dron920522@gmail.com>
2019-12-14 22:28:09 +0300

    Добавил задание 2 и 3

---
+++ Practice/A_makhov/Makhov2.py
@@ -0,0 +1,10 @@
+def c(a, b):
+    if a > b:
+        print(a)
+    else:
+        print(b)
+
+
+a = int(input("Введите первое число"))
+b = int(input("Введите второе число"))
+c(a, b)
---
+++ Practice/A_makhov/Makhov3.py
@@ -0,0 +1,10 @@
+def c(a, b):
+    if a > b:
+        return(a)
+    else:
+        return(b)
+
+
+a = int(input("Введите первое число"))
+b = int(input("Введите второе число"))
+print(c)
