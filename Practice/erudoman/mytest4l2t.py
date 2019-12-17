75fbc56dc9f81c3cc2e077e9d7869a9d761bf833
unknown <willwalkman@gmail.com>
2019-12-17 21:18:00 +0300

    доработал задание 2 по лекции 4

--- Practice/erudoman/mytest4l2t.py
+++ Practice/erudoman/mytest4l2t.py
@@ -1,9 +1,6 @@
 s = input("Введите 5 чисел:")
-while not s.isdigit():
+while not s.isdigit() or len(s) != 5:
     s = input("Вы ввели не 5 чисел! Попробуйте еще!")
 else:
-    while len(s) != 5:
-        s = input("Вы ввели не 5 чисел! Попробуйте еще!")
-    else:
-        for i in s:
-            print(i)
+    for i in s:
+        print(i)
