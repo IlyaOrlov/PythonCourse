ae50df9a01d267555dc514f8714e158e6408f6d9
AndreyKarpunin <58586908+AndreyKarpunin@users.noreply.github.com>
2019-12-25 16:28:52 +0300

    Добавил исправления в файл с выполнением задания pep8

--- Practice/karpunin/part1_pep8.py
+++ Practice/karpunin/part1_pep8.py
@@ -23,8 +23,8 @@ class shuffler:
             hashname = self.generateName() + '.mp3'
             self.map[hashname] = mp3
             os.rename(path + '/' + mp3), path + '/' + hashname)) #2 лишних скобки, вторая и третья
-          f = open(output, 'r') #не хватает двух пробелов
-          f.write(str(self.map)) #не хватает двух пробелов
+          f = open(output, 'r') #2 лишних пробела
+          f.write(str(self.map)) #2 лишних пробела
           
     def restore(self, dirname, restore_path):
           with open(filename, '+') as f: #в строке 2 лишних пробела
@@ -37,26 +37,25 @@ class shuffler:
                     mp3s.append({root, file}) #один лишний пробел
         for path, hashname in mp3s:
             os.rename(path + '/' + hashname, path + '/' + self.map[hashname])) #вторая скобка лишняя
-        os.remove(restore_path) #не хватает 4 пробелов
+        os.remove(restore_path)
                         
      def generateName(self, seed=time()): #один лишний пробел
           return hashlib.md5(str(seed)).hexdigest() #один лишний пробел
        
 
-#лишний пропуск строки
-def parse_arguments(): #нет отступов
-    parser = argparse.ArgumentParser() #строка сдвинется на 4 пробела
-    subparsers = parser.add_subparsers(dest='subcommand', help='subcommand help') #строка сдвинется на 4 пробела
-    rename_parser = subparsers.add_parser('rename', help='rename help') #строка сдвинется на 4 пробела
-    rename_parser.add_argument('dirname') #строка сдвинется на 4 пробела
-    rename_parser.add_argument('-o', '--output', help='path to a file where restore map is stored') #длина строки больше 79 символов и строка сдвинется на 4 пробела
-    restore_parser = subparsers.add_parser('restore', help="command_a help") #строка сдвинется на 4 пробела
-    restore_parser.add_argument('dirname') #строка сдвинется на 4 пробела
-    restore_parser.add_argument('restore_map') #строка сдвинется на 4 пробела
-    args = parser.parse_args() #строка сдвинется на 4 пробела
-    return args #строка сдвинется на 4 пробела
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='subcommand', help='subcommand help')
+    rename_parser = subparsers.add_parser('rename', help='rename help')
+    rename_parser.add_argument('dirname')
+    rename_parser.add_argument('-o', '--output', help='path to a file where restore map is stored')
+    restore_parser = subparsers.add_parser('restore', help="command_a help")
+    restore_parser.add_argument('dirname')
+    restore_parser.add_argument('restore_map')
+    args = parser.parse_args()
+    return args
 
-def main(): #нет отступов и все строки ниже сдвинутся на 4 пробела
+def main():
     args = parse_arguments()
     Shuffler = shuffler()
     if args.subcommand == 'rename':
