360ea427dbab7ec1d84db0597d78d3fde7085d74
Aleksandr Kruglov <aleksandr.kruglov@cadexchanger.com>
2019-12-30 20:20:12 +0300

    newT6

--- Practice/kruglov/home_task_4/PyLec4T6.py
+++ Practice/kruglov/home_task_4/PyLec4T6.py
@@ -1,16 +1,31 @@
 # 6 PyLecT6.py
 
 def del_find(el, mtx):
+    i = 0
+    x = []
     for row in mtx: 
-        if el in row:
-            x = row.index(el)
-    for row in mtx: 
-        row.remove(row[x])
-    return mtx
+        while i < len(row):
+            if el == row[i] and i not in x:
+                x.append(i)
+            i += 1
+        i = 0
+    x.sort()
+    for row1 in mtx:
+        cv = len(row)
+        while i < cv:
+            if row1[i] not in x:
+                print(row1[i])
+            # if row[z] not in x:
+            #     print(row[el])
+        # le = len(row)
+        # while i < le:
+            #if row[i] not in x[i]:
+                # row.pop(0)
+            i += 1
+        #i = 0
 
 
 if __name__ == "__main__":
-    matrix = [[1,2,3], [4,5,6],[7,8,9]]
-    element = 9
+    matrix = [[0,1,1], [0,5,6],[7,8,9]]
+    element = 1
     del_find(element, matrix)
-    print(matrix)
