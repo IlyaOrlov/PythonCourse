fe5bbcb85017259a9abb05f29d8ceac8ba6bed5b
Aleksandr <a.derendyaev@gmail.com>
2020-01-07 20:17:52 +0300

    Внёс решения задач 1 и 2 из практики второй лекции

---
+++ Practice/Derendyaev/task_2.1.py
@@ -0,0 +1,11 @@
+def fmax():
+    a = int(input("Введите первое число:"))
+    b = int(input("Введите второе число:"))
+    if a > b:
+        max = a
+    else:
+        max = b
+    return max
+
+
+print("Наибольшее число", fmax())
