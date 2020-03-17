8599e3ad852433a6ebc78a053f51bfa0ad3216e4
vlk6334 <62064636+vlk6334@users.noreply.github.com>
2020-03-17 13:06:05 +0300

    Added the first two tasks.

---
+++ Practice/m_kolodiev/2.1.py
@@ -0,0 +1,9 @@
+a = input("Input number 1: ")
+b = input("Input number 2: ")
+
+
+def maximum(f, s):
+    print(max(f, s))
+
+
+maximum(int(a), int(b))
---
+++ Practice/m_kolodiev/2.2.py
@@ -0,0 +1,10 @@
+a = input("Input number 1: ")
+b = input("Input number 2: ")
+
+
+def maximum(f, s):
+    m = max(f, s)
+    return m
+
+
+print(maximum(int(a), int(b)))
