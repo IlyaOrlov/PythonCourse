9b3ec70cebaa0c44046b3b8f2d5d4755ff038b73
Andrew Chubarovsky <62114376+Zondrillo@users.noreply.github.com>
2020-03-21 15:49:30 +0300

    4.2.1.py
    Добавил проверку на количество значений. Отформатировал вывод в соответствии с задачей.

--- Practice/a_chubarovsky/4.2.py
+++ Practice/a_chubarovsky/4.2.py
@@ -1,5 +1,9 @@
-a = 10819
+a = input('Введите пятизначное число: ')
 
-lst1 = str(a)
-for i in range(len(lst1)):
-    print(lst1[i])
+print('Число: {}'.format(a))
+lst1 = list(a)
+if len(lst1) == 5:
+    for i in lst1:
+        print("{} цифра равна {}".format((lst1.index(i) + 1), i))
+else:
+    print('Введённое число не пятизначное.')
