45e91ae434e732eb8fbaaa1f7734900f3d19108f
Andrew Chubarovsky <62114376+Zondrillo@users.noreply.github.com>
2020-03-21 22:59:22 +0300

    4.1.2.py
    Добавил конструкцию elif.

--- Practice/a_chubarovsky/4.1.py
+++ Practice/a_chubarovsky/4.1.py
@@ -1,11 +1,9 @@
 for i in range(1, 101):
     if i % 15 == 0:
         print('FizzBuzz')
+    elif i % 5 == 0:
+        print('Buzz')
+    elif i % 3 == 0:
+        print('Fizz')
     else:
-        if i % 5 == 0:
-            print('Buzz')
-        else:
-            if i % 3 == 0:
-                print('Fizz')
-            else:
-                print(i)
+        print(i)
