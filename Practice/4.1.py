9abbfcbe397f4205b1ef285e6333c8e520c0f673
vlk6334 <62064636+vlk6334@users.noreply.github.com>
2020-03-23 00:50:36 +0300

    Added three tasks from the fourth lecture. Fixed two tasks from the second lecture.

--- Practice/4.1.py
+++
@@ -1,9 +0,0 @@
-for x in range(1, 101):
-    if x % 15 == 0:
-        print('FizzBuzz')
-    elif x % 5 == 0:
-        print('Buzz')
-    elif x % 3 == 0:
-        print('Fizz')
-    else:
-        print(x)
--- Practice/4.3.py
+++
@@ -1,7 +0,0 @@
-arr = [0, 3, 24, 2, 3, 7]
-
-for i in range(len(arr)):
-    x = min(arr[i:])
-    j = arr[i:].index(x) + i
-    arr.insert(i, arr.pop(j))
-print(arr)
