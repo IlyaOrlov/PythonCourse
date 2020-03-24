c365f50354d9d0518bd3e706876e033e623b567f
Voronin Denis <vdv.92@inbox.ru>
2020-03-24 16:27:10 +0300

    Исправил третье задание, теперь работает

--- Practice/Voronin/practice2.py
+++ Practice/Voronin/practice2.py
@@ -2,11 +2,11 @@
 i = 1
 while i <= 100:
     if i % 15 == 0:
-        print('FizzBuzz')
+#         print('FizzBuzz')
     elif i % 5 == 0:
         print('Buzz')
     elif i % 3 == 0:
-        print('Fizz')
+#         print('Fizz')
     print(i)
     i += 1
 
