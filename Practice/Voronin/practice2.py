aef1629805d30e3abc4ebafb0ac33a74faa6322b
Voronin Denis <vdv.92@inbox.ru>
2020-03-24 18:03:01 +0300

    Исправил третье задание, теперь работает

--- Practice/Voronin/practice2.py
+++ Practice/Voronin/practice2.py
@@ -1,23 +1,23 @@
 # первое задание
-# i = 1
-# while i <= 100:
-#     if i % 15 == 0:
-#         print('FizzBuzz')
-#     elif i % 5 == 0:
-#         print('Buzz')
-#     elif i % 3 == 0:
-#         print('Fizz')
-#     print(i)
-#     i += 1
+i = 1
+while i <= 100:
+    if i % 15 == 0:
+        print('FizzBuzz')
+    elif i % 5 == 0:
+        print('Buzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    print(i)
+    i += 1
 
 # второе задание
-# number = input('Введите пятизначное число :')
-# print('Число :'+ number)
-# lst = list(number)
-# i = 0
-# while i <= len(lst):
-#     print("{} цифра равна : {}".format((i + 1), lst[i]))
-#     i += 1
+number = input('Введите пятизначное число :')
+print('Число :'+ number)
+lst = list(number)
+i = 0
+while i <= len(lst):
+    print("{} цифра равна : {}".format((i + 1), lst[i]))
+    i += 1
 
 
 # # Третье задание
