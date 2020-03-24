36b703dba9dd0dee8c371ebc19ec06695bbba237
AlexBogdanov01 <62241955+AlexBogdanov01@users.noreply.github.com>
2020-03-24 16:02:33 +0300

    Лекция №2 первое задание исправленный
    Исправленный вариант к заданию №1 лекция 2

---
+++ Practice/Lecture 2 Task 1 variant2.py
@@ -0,0 +1,12 @@
+a = int(input('insert a:'))
+b = int(input('insert b:'))
+
+
+def bolchee_chiclo(a, b):
+    if a > b:
+        print(a)
+    elif a <= 10:
+        print(b)
+    if a == b:
+        print('chicla ravni')
+        return
