b38588c4483385325f1d49e9d4f8eca1c13ecb23
AlexBogdanov01 <62241955+AlexBogdanov01@users.noreply.github.com>
2020-03-24 15:51:22 +0300

    Второе задание ко второй лекции
    Второй вариант задачи к лекции №2 с return

---
+++ Practice/Lecture 2.Task2.py
@@ -0,0 +1,13 @@
+def fun(a, b):
+    if a > b:
+        return a
+    elif a <= 10:
+        return b
+    if a == b:
+        return a == b
+    return
+
+
+print(fun(2, 5))
+print(fun(9, 11))
+print(fun(1, 1))
