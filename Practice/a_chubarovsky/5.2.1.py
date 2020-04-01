4960b73db0caf3b51ecbfd50e43a05aa06064995
Andrew Chubarovsky <62114376+Zondrillo@users.noreply.github.com>
2020-04-01 11:56:19 +0300

    5.2.1.py
    Импортировал класс Man из файла 5.1.py.

--- Practice/a_chubarovsky/5.2.1.py
+++ Practice/a_chubarovsky/5.2.1.py
@@ -1,13 +1,6 @@
 import random as rdm
 import time
-
-
-class Man:
-    def __init__(self, name):
-        self.name = name
-
-    def solve_task(self):
-        print("{} says: I'm not ready yet.".format(self.name))
+from 5.1 import Man
 
 
 class Pupil(Man):
