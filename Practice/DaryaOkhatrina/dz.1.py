4a612731c8832cd16f94fc9104d1de97181aba25
dokhatrina <dohatrina@mail.ru>
2020-04-01 11:20:59 +0300

    Коррективка dz.1 и dz 4.2

--- Practice/DaryaOkhatrina/dz.1.py
+++ Practice/DaryaOkhatrina/dz.1.py
@@ -1,18 +1,18 @@
-def bigger(a,b):
-    if a>b:
-        print (a)
+def bigger(a, b):
+    if a > b:
+        print(a)
     else:
         print(b)
 
-bigger(11,2)
-bigger(3,7)
+bigger(11, 2)
+bigger(3, 7)
 ###########################
-def bigger(a,b):
-    if a>b:
+def bigger(a, b):
+    if a > b:
         return a
     else:
         return b
 
-print(bigger(11,2))
-print(bigger(3,7))
+print(bigger(11, 2))
+print(bigger(3, 7))
 #############################
--- Practice/DaryaOkhatrina/dz.4.2.py
+++ Practice/DaryaOkhatrina/dz.4.2.py
@@ -1,7 +1,4 @@
-c = input('Введите  число: ')
+c = (input('Введите  число: '))
 print("Число {}".format(c))
-b = 0
-while b < len(c):
-    for i in range(len(c)):
-        b+=1
-        print('{} цифра равна  {}'.format(b,c[i]))
+for i in range(len(c)):
+    print('{} цифра равна  {}'.format(i+1, c[i]))
