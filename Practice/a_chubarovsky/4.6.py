ea47e645d77691fa6971813cab263ccc9a302656
Andrew Chubarovsky <62114376+Zondrillo@users.noreply.github.com>
2020-04-02 10:45:32 +0300

    4.6.1.py
    Изменил способ прохождения по матрице: с конца к началу.

--- Practice/a_chubarovsky/4.6.py
+++ Practice/a_chubarovsky/4.6.py
@@ -18,13 +18,11 @@ for i in array:
     print('{}'.format(i), end='')
     print()
 elem_to_del = int(input("Input an element to delete: "))
-k = 0
-for i in range(y):
-    for j in range(x):
-        if array[i][j-k] == elem_to_del:
-            column_to_del = [array[i][j-k], j-k]
+for i, e in reversed(list(enumerate(array))):
+    for j, f in reversed(list(enumerate(e))):
+        if array[i][j] == elem_to_del:
+            column_to_del = [array[i][j], j]
             del_column(array, column_to_del[1])
-            k += 1
 
 
 for i in array:
