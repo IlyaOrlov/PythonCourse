385f188f2c64d2f9d50119904507932809ac9cbf
artursidelnikov <sidelnikov.artur@inbox.ru>
2020-04-05 17:52:32 +0300

    4Lec6Zad

--- Practice/sidelnikov/4Lec6Zad.py
+++ Practice/sidelnikov/4Lec6Zad.py
@@ -1,42 +1,38 @@
-def Transport(matrix):
-    MatrTran = [None]*len(matrix[0])
+def transport(matrix):
+    if len(matrix) != 0:
+        matrtran = [None]*len(matrix[0])
 
-    for i in range(len(MatrTran)):
-        MatrTran[i] =  [None]*len(matrix)
+        for i in range(len(matrtran)):
+            matrtran[i] =  [None]*len(matrix)
 
-    for i in range(len(matrix[0])):
-        for j in range(len(matrix)):
-            MatrTran[i][j] = matrix[j][i]
-    return MatrTran
-
-
-def udalenie(MAT):
-    matrix = []
-    k = 0
-    for i in range(len(MAT)):#строка
-        if MAT[i].count(chislo) == 0:
-            matrix.append(MAT[i])
-            k += 1
-    if k == 0:
-        return 0
+        for i in range(len(matrix[0])):
+            for j in range(len(matrix)):
+                matrtran[i][j] = matrix[j][i]
+        return matrtran
     else:
         return matrix
+
+
+def udalenie(mat):
+    matrix = []
+    for i in range(len(mat)):#строка
+        if mat[i].count(chislo) == 0:
+            matrix.append(mat[i])
+
+    return matrix
 
 
 matrix = [[8, 2, 1, 3, 6],
           [4, 2, 1, 1, 7],
           [7, 6, 7, 3, 1],
           [1, 1, 8, 6, 4]]
+print("Матрица", matrix)
 chislo = int(input("Введите число:"))
-MatrTran = Transport(matrix)
-matrix1 = udalenie(MatrTran)
-print("Матрица:", matrix)
-print("Транспонированнная матрица:", MatrTran)
-if matrix1 == 0:
-    print("Ответ:", matrix1)
-else:
-    matrix2 = Transport(matrix1)
-    print("Матрица без строк которые имеют число: ", matrix1)
-    print("Ответ:", matrix2)
+matrtran = transport(matrix)
+matrix1 = udalenie(matrtran)
+print("Транспонированнная матрица:", matrtran)
+matrix2 = transport(matrix1)
+print("Матрица без строк которые имеют число {}: ".format(chislo), matrix1)
+print("Ответ:", matrix2)
 
 
