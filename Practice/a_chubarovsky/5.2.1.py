e2a97b11ec47446b43ebb558c4df5fc7172cc2b2
Andrew Chubarovsky <62114376+Zondrillo@users.noreply.github.com>
2020-04-05 22:41:22 +0300

    5.2.2.py
    Добавил пример использования: вызов метода solve_task для экземпляров обоих классов Man и Pupil.

--- Practice/a_chubarovsky/5.2.1.py
+++ Practice/a_chubarovsky/5.2.1.py
@@ -7,3 +7,9 @@ class Pupil(Man):
     def solve_task(self):
         time.sleep(rdm.randint(3, 6))
         print("{} says: I'm not ready yet.".format(self.name))
+
+        
+m = Man('Misha')
+print(m.solve_task())
+p = Pupil('Ivan')
+print(p.solve_task())
