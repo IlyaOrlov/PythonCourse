b1360f0debe4ba9bcac720189bd3cd235619be29
dokhatrina <dohatrina@mail.ru>
2020-04-07 00:53:17 +0300

    исправлено 4.5

--- Practice/DaryaOkhatrina/dz.4.5.py
+++ Practice/DaryaOkhatrina/dz.4.5.py
@@ -1,14 +1,13 @@
 s = 'cat is better than dog'
 d = {'cat': 'кошка','dog': 'собака'}
 for i in d:
-   s = s.replace(i, (d[i]))
+    s = s.replace(i, (d[i]))
 print(s)
 
 s = 'cat is better than dog'
-d = {'cat': 'кошка','dog': 'собака'}
 s1 = s.split(' ')# преобразуем в список
 for i in range(len(s1)):#проходим по списку с помощью цикла
-    if s1[i] in d: # если индекс элемента списка принадлежит словарю
+    if s1[i] in d:
         s1[i] = d[s1[i]]
 print(' '.join(s1))# преобразуем обратно в строку
 
