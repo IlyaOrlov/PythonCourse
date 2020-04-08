dba11f03af2c328c05615e939bdb8d910044f3e9
Serj52 <blackday52@mail.ru>
2020-04-08 15:10:30 +0300

    Ver_2

--- Practice/tsyganov/Zad_4(Lect_4).py
+++ Practice/tsyganov/Zad_4(Lect_4).py
@@ -1,16 +1,17 @@
-def zamena():
+def zamena(self,number):
+    self.number=number
     lst = []
     f = open(r'C:\downlods\Пример_1.txt', 'r+')
     for i in f:
-        if g == 1:
+        if  self.number == 1:
             i = i.replace('\\t', '    ')
             lst.append(i)
             print(lst)
-        elif g == 2:
+        elif  self.number == 2:
             i = i.replace('\\t', '    ')
             lst.append(i)
             print(lst)
     f.close()
 
-g = input('Введите число 1 (замена табуляции) или 2 (замена пробелов): ')  # строка в файле'\tspa\t\tmg    hdfdf fgfg\t'
-zamena()
+number = input('Введите число 1 (замена табуляции) или 2 (замена пробелов): ')  # строка в файле'\tspa\t\tmg    hdfdf fgfg\t'
+zamena(number)
