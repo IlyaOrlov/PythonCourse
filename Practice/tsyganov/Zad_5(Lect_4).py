1b4833c4174b46398346e00fe59e32f52bd2175e
Serj52 <blackday52@mail.ru>
2020-04-08 15:11:53 +0300

    Ver_2

--- Practice/tsyganov/Zad_5(Lect_4).py
+++ Practice/tsyganov/Zad_5(Lect_4).py
@@ -3,10 +3,10 @@ def funk(my_str,my_typle):
     my_str=my_str.split(' ')
     i=0
     while i<len(my_str):
-        for l in my_typle:
-            if my_str[i]==l:
-                my_str[i]=my_typle.get(l)
-        i+=1
+            if my_str[i] in my_typle:
+                my_str[i]=my_typle.get(my_str[i])
+            i+=1
+
     my_str=' '.join(my_str)
     print(my_str)
 
