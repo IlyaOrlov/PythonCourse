915b516b8eace52bbd0ba31d930ff302872a6ba8
Andrew Chubarovsky <62114376+Zondrillo@users.noreply.github.com>
2020-04-12 16:06:23 +0300

    6.3.1.py
    Встроил @bencmark в тело контекст менеджера.

--- Practice/a_chubarovsky/6.3.py
+++ Practice/a_chubarovsky/6.3.py
@@ -1,31 +1,26 @@
 import time
-
-
-def benchmark(func):
-    def wrapper(*args, **kwargs):
-        t = time.process_time()
-        res = func(*args, **kwargs)
-        print("{} spent {}".format(func.__name__, time.process_time() - t))
-        return res
-    return wrapper
+import sys
 
 
 class MyContextManager:
-    def __init__(self, file_name):
+    def __init__(self, file_name, func_name):
         self.file_name = file_name
+        self.func_name = func_name
 
     def __enter__(self):
         self.file = open(self.file_name, "w+")
+        self.t = time.process_time()
         return self.file
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        print("{} spent {}".format(self.func_name.__name__, time.process_time() - self.t))
         self.file.close()
 
 
-@benchmark
 def new_func(file_name):
-    with MyContextManager(file_name) as f:
-        f.write("Hello World!!!")
+    with MyContextManager(file_name, new_func) as f:
+        x = 'Spam!'
+        f.write(f"{sys.platform}\n{str(2**100)}\n{str(x * 10)}")
         data = f.read()
     return data
 
