5c03163c7cc980ef5e969ca6da3a68724eff459e
dokhatrina <dohatrina@mail.ru>
2020-04-12 23:55:03 +0300

    + 5.2

--- Practice/DaryaOkhatrina/dz.5.1.py
+++ Practice/DaryaOkhatrina/dz.5.1.py
@@ -1,7 +1,20 @@
+import time
+import random as r
+
+
 class Man:
     def __init__(self, name):
         self.name = name
     def solve_task(self):
         print(self.name, "I'm not ready yet")
+
+class Pupil(Man):
+    def solve_task(self):
+        print(self.name, "I'm not ready yet", time.sleep, r.randint(3, 6))
+
+
+
 x = Man('Ivan')
 x.solve_task()
+y = Pupil('Sasha')
+y.solve_task()
