835fb20035be8947ed76d820c61c0ec2fffc32ab
ilya.orlov <orlov.soft.company@gmail.com>
2020-04-21 17:39:59 +0300

    Update for Lec9

--- Tasks/for_lec9/RLockExample2.py
+++ Tasks/for_lec9/RLockExample2.py
@@ -6,29 +6,30 @@ class Base:
     y = 0
 
 
-def print_fun(l):
+def print_fun(lock):
     i = 0
     while i < 2:
-        l.acquire()
+        lock.acquire()
         Base.x = input("2x: ")
         Base.y = input("2y: ")
-        l.release()
         print(f'Thread 2: Base.x = {Base.x}, Base.y = {Base.y}')
+        lock.release()
         i += 1
-        time.sleep(5)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
-    l = RLock()
-    t = Thread(target=print_fun, args=(l,))
+    lock = RLock()
+    t = Thread(target=print_fun, args=(lock,))
     t.start()
     i = 0
     while i < 2:
-        l.acquire()
+        lock.acquire()
         Base.x = input("1x: ")
         Base.y = input("1y: ")
-        l.release()
         print(f'Thread 1: Base.x = {Base.x}, Base.y = {Base.y}')
+        lock.release()
         i += 1
-        time.sleep(5)
-t.join()
+        time.sleep(1)
+    t.join()
+    
