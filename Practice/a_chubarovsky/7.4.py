ac4c3869745be22177d29e4557fc7a08c97890ad
Andrew Chubarovsky <62114376+Zondrillo@users.noreply.github.com>
2020-04-22 23:11:58 +0300

    7.4.1.py
    Модуль datetime заменёна на time.
    Тело циклов file in files и directory in dirs вынесено в отдельную функцию walker.

--- Practice/a_chubarovsky/7.4.py
+++ Practice/a_chubarovsky/7.4.py
@@ -1,34 +1,39 @@
 import os
 import shutil as sh
-import datetime as dt
+import time
+
+
+def walker(const_path, temp_path, file_or_dir):
+    file_or_dir_path = os.path.join(temp_path, file_or_dir)
+    file_or_dir_exist_time = time.time() - os.path.getctime(file_or_dir_path)
+    diff_time = os.path.getctime(file_or_dir_path) - os.path.getctime(temp_path)  # разница во времени существования
+    # папки и вложенных в неё файлов
+    if os.path.isfile(file_or_dir):
+        if file_or_dir_exist_time > 60:
+            os.remove(file_or_dir_path)
+        elif diff_time > 60 and file_or_dir_path != const_path:
+            upper_file = os.path.split(temp_path)
+            sh.move(file_or_dir_path, upper_file[0])  # перемещение файла на уровень выше
+    elif os.path.isdir(file_or_dir):
+        if file_or_dir_exist_time > 120:
+            os.rmdir(file_or_dir_path)
+        elif diff_time > 90 and file_or_dir_path != const_path:
+            upper_dir = os.path.split(temp_path)
+            sh.move(file_or_dir_path, upper_dir[0])  # перемещение папки на уровень выше
 
 
 def search_and_delete(some_path):
     for root, dirs, files in os.walk(some_path, topdown=False):
         for file in files:
-            file_path = os.path.join(root, file)
-            file_exist_time = dt.datetime.now() - dt.datetime.fromtimestamp(
-                os.path.getctime(file_path))  # время существования файла
-            diff_time = dt.datetime.fromtimestamp(os.path.getctime(file_path)) - dt.datetime.fromtimestamp(
-                os.path.getctime(root))  # разница во времени существования папки и вложенных в неё файлов
-            if file_exist_time > dt.timedelta(seconds=60):
-                os.remove(file_path)
-            elif diff_time > dt.timedelta(seconds=60) and file_path != some_path:
-                upper_file = os.path.split(root)
-                sh.move(file_path, upper_file[0])  # перемещение файла на уровень выше
+            walker(some_path, root, file)
         for directory in dirs:
-            dir_path = os.path.join(root, directory)
-            dir_exist_time = dt.datetime.now() - dt.datetime.fromtimestamp(os.path.getctime(dir_path))
-            diff_time = dt.datetime.fromtimestamp(os.path.getctime(dir_path)) - dt.datetime.fromtimestamp(
-                os.path.getctime(root))  # разница во времени существования папки и вложенных в неё папок
-            if dir_exist_time > dt.timedelta(seconds=120):
-                os.rmdir(dir_path)
-            elif diff_time > dt.timedelta(seconds=90) and dir_path != some_path:
-                upper_dir = os.path.split(root)
-                sh.move(dir_path, upper_dir[0])  # перемещение файла на уровень выше
+            walker(some_path, root, directory)
 
 
 if __name__ == '__main__':
     path = r'C:\\Users\Current\PycharmProjects\myproject\dir_to_delete'
     while True:
-        search_and_delete(path)
+        try:
+            search_and_delete(path)
+        except KeyboardInterrupt:
+            print('Stopped by User.')
