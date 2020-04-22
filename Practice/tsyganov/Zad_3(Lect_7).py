d56fafd6f403ad7c1da1d9ba4fbcfb0d4ee08d9f
Serj52 <blackday52@mail.ru>
2020-04-23 02:25:28 +0300

    Ver_1

---
+++ Practice/tsyganov/Zad_3(Lect_7).py
@@ -0,0 +1,29 @@
+import random
+import pickle
+class Human:
+
+    def __init__(self,name,surname):
+        self.name=name
+        self.surname=surname
+    def __repr__(self):
+        res = self.name + ' ' + self.surname
+        return res
+
+def serialize(self):
+        with open('myfile.py','ab') as f:
+            res_ser=pickle.dump(self, f)
+            return res_ser
+
+def deserialize():
+        with open("myfile.py", 'rb') as f:
+                res_des = pickle.load(f)
+                print (res_des)
+
+def zavod(n):
+    surname_box=['Lennon','Hendrix','Morrison']
+    name_box= ['John','Jimi','Jim']
+    res= [Human(random.choice(surname_box),random.choice(name_box)) for x in range(n)]
+    return res
+
+serialize(zavod(20))
+deserialize()
