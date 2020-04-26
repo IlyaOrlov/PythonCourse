cd72a65b31ca7ba9eb3de5c179e8acaaf4964522
Andrew Chubarovsky <62114376+Zondrillo@users.noreply.github.com>
2020-04-27 00:29:39 +0300

    9.2.1.py
    Скорректировано с учётом замечаний.

--- Practice/a_chubarovsky/9.2.py
+++ Practice/a_chubarovsky/9.2.py
@@ -1,51 +1,42 @@
-from multiprocessing import Process, Queue, current_process
+import multiprocessing as mp
 import time
 import itertools as it
 
 
-def my_sum_int(q, data):
-    sum = 0
-    for element in data:
-        q.put(element)
-        if type(element) == int:
-            sum += element
-            print(f"{current_process().name} processed values {element}")
-    print(f"{current_process().name} finished with result = {sum}.")
+sum_list = [0, '', []]
 
 
-def my_sum_str(q, data):
-    sum = ''
-    for element in data:
-        q.put(element)
-        if type(element) == str:
-            sum += ' ' + element
-            print(f"{current_process().name} processed values {element}")
-    print(f"{current_process().name} finished with result = {sum}.")
-
-
-def my_sum_lst(q, data):
-    sum = []
-    for element in data:
-        q.put(element)
-        if type(element) == list:
-            sum += list(it.chain(element))
-            print(f"{current_process().name} processed values {element}")
-    print(f"{current_process().name} finished with result = {sum}.")
+def my_sum(q):
+    while True:
+        arg = q.get()
+        proc_name = mp.current_process().name
+        if type(arg) == int:
+            sum_list[0] += arg
+            print(f"{proc_name} processed value {arg}")
+        if type(arg) == str:
+            sum_list[1] += ' ' + arg
+            print(f"{proc_name} processed value {arg}")
+        if type(arg) == list:
+            sum_list[2] += list(it.chain(arg))
+            print(f"{proc_name} processed value {arg}")
+        time.sleep(2)
+        if arg is None:
+            break
+    print(sum_list)
 
 
 if __name__ == '__main__':
     start = time.time()
-    q = Queue()
+    q = mp.JoinableQueue()
     data = [1, 2, 3, 'Hello', 'World', [1, 2, 3, 9], [4, 5, 6, 10], 5, 11, 'Something', [4, 8, 4], 'How are you?', 100,
             100, [1, 1, 1, 1, 1]]
-    p1 = Process(target=my_sum_int, args=(q, data))
-    p2 = Process(target=my_sum_str, args=(q, data))
-    p3 = Process(target=my_sum_lst, args=(q, data))
-    p1.start()
-    p2.start()
-    p3.start()
-    q.close()
-    p1.join()
-    p2.join()
-    p3.join()
+    num_proc = 3
+    processes = [mp.Process(target=my_sum, args=(q,)) for i in range(num_proc)]
+    for proc in processes:
+        proc.start()
+    for element in data:
+        q.put(element)
+    for proc in processes:
+        q.put(None)
+        proc.join()
     print("Lead time = {:.2f} sec.".format(time.time() - start))
