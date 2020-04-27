03d5fe7c027ac50dd75889662429e8799ca72187
Andrew Chubarovsky <62114376+Zondrillo@users.noreply.github.com>
2020-04-27 14:23:03 +0300

    9.2.2.py
    Очередь исключена из кода. Результаты вычислений выводятся отдельно для каждого потока.

--- Practice/a_chubarovsky/9.2.py
+++ Practice/a_chubarovsky/9.2.py
@@ -6,10 +6,9 @@ import itertools as it
 sum_list = [0, '', []]
 
 
-def my_sum(q):
-    while True:
-        arg = q.get()
-        proc_name = mp.current_process().name
+def my_sum(data):
+    proc_name = mp.current_process().name
+    for arg in data:
         if type(arg) == int:
             sum_list[0] += arg
             print(f"{proc_name} processed value {arg}")
@@ -19,24 +18,23 @@ def my_sum(q):
         if type(arg) == list:
             sum_list[2] += list(it.chain(arg))
             print(f"{proc_name} processed value {arg}")
-        time.sleep(2)
-        if arg is None:
-            break
-    print(sum_list)
+        time.sleep(1)
+    if proc_name == 'Process-1':
+        print(f"{proc_name} finished with result: {sum_list[0]}")
+    if proc_name == 'Process-2':
+        print(f"{proc_name} finished with result: {sum_list[1]}")
+    if proc_name == 'Process-3':
+        print(f"{proc_name} finished with result: {sum_list[2]}")
 
 
 if __name__ == '__main__':
     start = time.time()
-    q = mp.JoinableQueue()
     data = [1, 2, 3, 'Hello', 'World', [1, 2, 3, 9], [4, 5, 6, 10], 5, 11, 'Something', [4, 8, 4], 'How are you?', 100,
             100, [1, 1, 1, 1, 1]]
     num_proc = 3
-    processes = [mp.Process(target=my_sum, args=(q,)) for i in range(num_proc)]
+    processes = [mp.Process(target=my_sum, args=(data,)) for i in range(num_proc)]
     for proc in processes:
         proc.start()
-    for element in data:
-        q.put(element)
     for proc in processes:
-        q.put(None)
         proc.join()
     print("Lead time = {:.2f} sec.".format(time.time() - start))
