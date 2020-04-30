6672d64642da055038def5f026540957e4fd3013
Voronin <vdv.92@inbox.ru>
2020-04-30 09:15:41 +0300

    3-е задание из 7-ой лекции на проверку, сделал с проверкой, все получилось

--- Practice/Voronin/7.3.py
+++ Practice/Voronin/7.3.py
@@ -8,15 +8,20 @@ class Human():
         self.age = age
         self.hair_color = hair_color
         self.p_of_residence = p_of_residence
+    def __str__(self):
+        return f"Human: first_name={self.first_name}, last_name={self.last_name}, age={self.age}, hair_color={self.hair_color}," \
+               f" p_of_residence={self.p_of_residence}"
 
 def fun(num):
     f_name = ['Denis', 'Ksenia', 'Alex', 'Ivan', 'Ilya']
-    l_name = ['Kakayto', 'Drugayto', 'Escho', 'Sloghno', 'Pupkin']
+    l_name = ['Moirov', 'Shoikhet', 'Vasin', 'Riviera', 'Pupkin']
     p_of_r = ['Moscow', 'Nizhny Novgorod', 'Rybinsk', 'St. Petersburg', 'California']
     hair_color = ['Green', 'Red', 'Blue', 'Gray', 'Black']
     people = []
     for i in range(num):
         people.append(Human(random.choice(f_name), random.choice(l_name), random.randint(1, 115), random.choice(hair_color), random.choice(p_of_r)))
+    for i in people:
+        print(i)
     with open('data.pickle', 'wb') as f:
         pickle.dump(people, f)
     return people
@@ -24,10 +29,12 @@ def fun(num):
 def fun2(file):
     with open(file, 'rb') as f:
         data_new = pickle.load(f)
+    for line in data_new:
+        print(line)
     return data_new
 
 
-fun(4)
+fun(5)
 fun2('data.pickle')
 
 
