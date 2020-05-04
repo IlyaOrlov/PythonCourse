374d503be434725297ea29128b07b646cee779da
Serj52 <blackday52@mail.ru>
2020-05-04 18:59:30 +0300

    Ver_3

--- Practice/tsyganov/Zad_5(Lect_4).py
+++ Practice/tsyganov/Zad_5(Lect_4).py
@@ -1,16 +1,15 @@
+def funk(my_str, my_typle):
+    my_str = my_str.split(' ')
+    i = 0
+    while i < len(my_str):
+        if my_str[i] in my_typle:
+            my_str[i] = my_typle.get(my_str[i])
+            i += 1
 
-def funk(my_str,my_typle):
-    my_str=my_str.split(' ')
-    i=0
-    while i<len(my_str):
-            if my_str[i] in my_typle:
-                my_str[i]=my_typle.get(my_str[i])
-            i+=1
-
-    my_str=' '.join(my_str)
+    my_str = ' '.join(my_str)
     print(my_str)
 
-my_str='Я учусь писать на языке Пайтон'
-my_typle={'Пайчарм': 'Pycharm','Пайтон':'Python'}
+my_str = 'Я учусь писать на языке Пайтон'
+my_typle = {'Пайчарм': 'Pycharm', 'Пайтон':'Python'}
 
-funk(my_str,my_typle)
+funk(my_str, my_typle)
