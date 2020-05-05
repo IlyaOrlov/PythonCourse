73a1e3323bba8a995b1a16e4f1733ded7059c344
Andrew Chubarovsky <52russiadk@gmail.com>
2020-05-05 15:30:41 +0300

    11.1.py
    Задание 1 из лекции №11.

---
+++ Practice/a_chubarovsky/11.1.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+
+class MySqlite:
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        print(f'DataBase {self.db_name} is open.')
+        self.cursor = self.conn.cursor()
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('DataBase is closed.')
+        self.conn.close()
+
+    def execute(self, some_cmd):
+        self.cursor.executescript(some_cmd)
+        if 'CREATE' in some_cmd:
+            print('Table created.')
+        elif 'INSERT' in some_cmd:
+            print('Data added into Table.')
+        elif 'UPDATE' in some_cmd:
+            print('Table changed successfully.')
+        elif 'DROP' in some_cmd:
+            print('Table was deleted.')
+        self.conn.commit()
+
+    def select(self, some_cmd):
+        c = self.cursor.execute(some_cmd)
+        for row in c:
+            print(row)
+
+
+if __name__ == '__main__':
+    try:
+        table = MySqlite('my_sqlite_database.db')
+        with table:
+            while True:
+                cmd = input('Type SQL command: ')
+                if cmd == 'quit':
+                    break
+                elif 'SELECT' in cmd:
+                    table.select(cmd)
+                elif 'SELECT' not in cmd and cmd != 'quit':
+                    table.execute(cmd)
+    except Exception as Error:
+        print(f'Error: {Error}')
