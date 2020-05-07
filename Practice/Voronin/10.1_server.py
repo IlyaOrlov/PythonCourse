80bd603f2950df1d935fee9545b309a9a0e71bdf
Voronin <vdv.92@inbox.ru>
2020-05-07 09:17:09 +0300

    1-ое задание из 10-ой лекции, сервер. Поправил замечания по двум вопросам, на третий пока не знаю ответа

--- Practice/Voronin/10.1_server.py
+++ Practice/Voronin/10.1_server.py
@@ -1,6 +1,6 @@
 import socket
 
-dict = {'0': 'а', '1': 'е', '2': 'и', '3': 'о'}
+d = {'0': 'а', '1': 'е', '2': 'и', '3': 'о'}
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '127.0.0.1'
 port = 12345
@@ -11,18 +11,10 @@ while True:
     f = conn.recv(1024)
     f = f.decode()
     a = ''
-    for x,i in enumerate(f):
-        if i == '0':
-            a += dict[i]
-        elif i == '1':
-            a += dict[i]
-        elif i == '2':
-            a += dict[i]
-        elif i == '3':
-            a += dict[i]
-        else:
-            a += f[x]
-    conn.send(a.encode())
+    for x in d.keys():
+        f = f.replace(x, d[x])
+
+    conn.send(f.encode())
     conn.close()
 
 
