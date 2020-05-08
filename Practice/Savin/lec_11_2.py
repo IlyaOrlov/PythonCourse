823c2d9d3503f06672e09ce5e6a2189fb4fe55ff
SavinVV <v.savin94@mail.ru>
2020-05-08 13:07:28 +0400

    Lec_11_2 cor

--- Practice/Savin/lec_11_2.py
+++ Practice/Savin/lec_11_2.py
@@ -1,5 +1,5 @@
 import collections
-from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String,  ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.schema import CreateTable
@@ -21,9 +21,14 @@ class Manufacturer(Base):
         return f'Manufacturer: {self.brand}'
 
 
-association_table = Table('association', Base.metadata,
-    Column('customer_id', Integer, ForeignKey('customer.id')),
-    Column('product_id', Integer, ForeignKey('product.id')))
+class Association(Base):
+    __tablename__ = 'association'
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customer.id'))
+    product_id = Column(Integer, ForeignKey('product.id'))
+
+    def __repr__(self):
+        return f'Customer_id: {self.customer_id}. Product_id: {self.product_id}'
 
 
 class Product(Base):
@@ -35,7 +40,7 @@ class Product(Base):
     manufacturer = relationship("Manufacturer", back_populates="products")
     customers = relationship(
         "Customer",
-        secondary=association_table,
+        secondary='association',
         back_populates="products")
 
     def __repr__(self):
@@ -48,7 +53,7 @@ class Customer(Base):
     name = Column(String)
     products = relationship(
         "Product",
-        secondary=association_table,
+        secondary='association',
         back_populates="customers")
 
     def __repr__(self):
@@ -60,7 +65,7 @@ Base.metadata.create_all(engine)
 print(CreateTable(Manufacturer.__table__).compile(engine))
 print(CreateTable(Product.__table__).compile(engine))
 print(CreateTable(Customer.__table__).compile(engine))
-print(CreateTable(association_table).compile(engine))
+print(CreateTable(Association.__table__).compile(engine))
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -68,7 +73,8 @@ session = Session()
 session.add_all([
     Manufacturer(brand='Sony'),
     Manufacturer(brand='Samsung'),
-    Manufacturer(brand='Apple')])
+    Manufacturer(brand='Apple'),
+    Manufacturer(brand='LG')])
 
 session.commit()
 
@@ -81,7 +87,10 @@ session.add_all([
     Product(name='samsung laptop', price=30, manufacturer_id=2),
     Product(name='apple TV', price=17, manufacturer_id=3),
     Product(name='apple smartphone', price=30, manufacturer_id=3),
-    Product(name='apple laptop', price=50, manufacturer_id=3)])
+    Product(name='apple laptop', price=50, manufacturer_id=3),
+    Product(name='LG TV', price=17, manufacturer_id=3),
+    Product(name='LG smartphone', price=30, manufacturer_id=3),
+    Product(name='LG laptop', price=50, manufacturer_id=3)])
 
 session.commit()
 
@@ -94,48 +103,56 @@ session.add_all([
 
 session.commit()
 
-session.execute("INSERT INTO association "
-                "VALUES (1, 5), (2, 4), (3, 1), (4, 1), (1, 2), (2, 5), (3, 9), (4, 5)")
+session.add_all([
+    Association(customer_id=1, product_id=5),
+    Association(customer_id=2, product_id=4),
+    Association(customer_id=3, product_id=1),
+    Association(customer_id=4, product_id=1),
+    Association(customer_id=1, product_id=2),
+    Association(customer_id=2, product_id=5),
+    Association(customer_id=3, product_id=9),
+    Association(customer_id=4, product_id=5)])
 
+session.commit()
 
 c = collections.Counter()
 for item in session.query(Product).filter(Product.price < 10):
     c[item.manufacturer_id] += 1
-for i, j in c.items():
-    if j > 2:
-        for item in session.query(Manufacturer).filter(Manufacturer.id == i):
+for brand_id, values in c.items():
+    if values > 2:
+        for item in session.query(Manufacturer).filter(Manufacturer.id == brand_id):
             brand = item.brand
-            print(f'{brand} has {j} products cheaper than 10')
+            print(f'{brand} has {values} products cheaper than 10')
 
 lst = []
-for item in session.execute("SELECT * FROM association"):
+for item in session.query(Association):
     lst.append(item)
 
 d = dict()
 for i in lst:
-    for item in session.query(Product).filter(Product.id == i[1]):
+    for item in session.query(Product).filter(Product.id == i.product_id):
         brand = item.manufacturer_id
         if brand not in d:
             d[brand] = list()
 
 for i in lst:
-    for item in session.query(Product).filter(Product.id == i[1]):
+    for item in session.query(Product).filter(Product.id == i.product_id):
         brand = item.manufacturer_id
-        if i[0] not in d[brand]:
-            d[brand].append(i[0])
+        if i.customer_id not in d[brand]:
+            d[brand].append(i.customer_id)
 
-for i, j in d.items():
-    for item in session.query(Manufacturer).filter(Manufacturer.id == int(i)):
+for brand_id, customers in d.items():
+    for item in session.query(Manufacturer).filter(Manufacturer.id == int(brand_id)):
         brand = item.brand
     names_customers = []
-    for id in j:
+    for id in customers:
         for item in session.query(Customer).filter(Customer.id == id):
             name = item.name
             names_customers.append(name)
     print(f'Brand: {brand}. Customers: {" ".join(names_customers)}')
 
 product_counter = collections.Counter()
-for item in session.execute("SELECT * FROM association"):
+for item in session.query(Association):
     product_counter[item.product_id] += 1
 
 top_product = {}
@@ -151,3 +168,24 @@ for product_id, c in product_counter.items():
 for brand, product in top_product.items():
     print(f'{brand}. Top product: {product}')
 
+brands = []
+for product_id in product_counter.keys():
+    for product in session.query(Product).filter(Product.id == int(product_id)):
+        if product.manufacturer_id not in brands:
+            brands.append(product.manufacturer_id)
+
+for i in brands:
+    for item in session.query(Manufacturer).filter(Manufacturer.id == i):
+        brand = item.brand
+        brand_id = item.id
+        print(f'{brand}:')
+        for product_id, values in product_counter.items():
+            for product in session.query(Product).filter(Product.id == int(product_id)):
+                if product.manufacturer_id == brand_id:
+                    product_name = product.name
+                    profit = product.price * values
+                    print(f'{product_name}: profit {profit}')
+
+for item in session.query(Manufacturer):
+    if item.id not in brands:
+        print(f'{item.brand} profit 0')
