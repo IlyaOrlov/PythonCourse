6735eb885a7205fd6aad415ae37565e2c2c28183
vlk6334 <62064636+vlk6334@users.noreply.github.com>
2020-05-09 22:00:54 +0300

    Fixed 10.2 task.

--- Practice/m_kolodiev/t10_2_client.py
+++ Practice/m_kolodiev/t10_2_client.py
@@ -10,7 +10,7 @@ class User:
 
 if __name__ == '__main__':
     users = {'Henry': 29, 'John': 50, 'Oliver': 33, 'Stanley': 27}
-    for u in users:
+    for name, age in users.items():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(('127.0.0.1', 12345))
-        s.send(pickle.dumps(User(u, users[u])))
+        s.send(pickle.dumps(User(name, age)))
