e56961a13732f2f4ff6b18c60c1cbab9c605c4f9
Voronin <vdv.92@inbox.ru>
2020-05-11 13:15:34 +0300

    добавил метод repr, но я так понимаю сервер не импортирует класс Users... в итоге я получаю тип объекта и адрес его

--- Practice/Voronin/10.2_client.py
+++ Practice/Voronin/10.2_client.py
@@ -2,12 +2,12 @@ import socket
 import pickle
 from users import Users
 
-ivan = Users('Ivan', ' 37')
-sergei = Users('Sergei', ' 21')
+ivan = Users('Ivan', '37')
+sergei = Users('Sergei', '21')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '127.0.0.1'
 port = 8787
 s.connect((host, port))
-s.send(pickle.dumps(repr(ivan)))
+s.send(pickle.dumps(ivan))
 s.close()
--- Practice/Voronin/users.py
+++ Practice/Voronin/users.py
@@ -2,3 +2,5 @@ class Users:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    def __repr__(self):
+        return 'User with name {} and age {}'.format(self.name, self.age)
