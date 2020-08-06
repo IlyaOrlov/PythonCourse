8d11e42f84d44adc6e3fa24990a5865b7155e657
AzamatMuratshin <azketc4@gmail.com>
2020-08-06 17:55:46 +0300

    pep8

--- Tasks/pep8task.py
+++ Tasks/pep8task.py
@@ -1,18 +1,21 @@
-import sys
-import os
+import os               # порядок импорты стандартной библиотеки \ сторонних библиотек \ импорты модулей текущего проекта
+import sys              # пробелы между группами импорта
+
 import hashlib
 import ast
 import argparse
+
+from fileinput import filename
 from time import *
+                        # 2 строки между блоком import и началом блока кода
 
-
-class shuffler:
+class Shuffler:         # имена классов вида CamelCase
 
     def __init__(self):
         self.map = {}
 
     def rename(self, dirname, output):
-          mp3s = []
+        mp3s = []                                      # выравнивание-отступы
         for root, directories, files in os.walk(dirname):
             for file in files:
                 if file[-3:] == '.mp3':
@@ -20,24 +23,24 @@ class shuffler:
         for path, mp3 in mp3s:
             hashname = self.generateName() + '.mp3'
             self.map[hashname] = mp3
-            os.rename(path + '/' + mp3), path + '/' + hashname))
-          f = open(output, 'r')
-          f.write(str(self.map))
+            os.rename(path + '/' + mp3, path + '/' + hashname)       # два лишних закрывающих пробела в строке
+        f = open(output, 'r')                           # выравнивание-отступы
+        f.write(str(self.map))
 
     def restore(self, dirname, restore_path):
-          with open(filename, '+') as f:
+        with open(filename, '+') as f:                  # использование filename без импорта
             self.map = ast.literal_eval(f.read())
-          mp3s = []
+        mp3s = []                                       # выравнивание-отступы
         for root, directories, files in os.walk(dirname):
             for file in files:
                if file[-3:] == '.mp3':
                     mp3s.append({root, file})
         for path, hashname in mp3s:
-            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
-        os.remove(restore_path)
-                
-     def generateName(self, seed=time()):
-          return hashlib.md5(str(seed)).hexdigest()
+            os.rename(path + '/' + hashname, path + '/' + self.map[hashname])     # лишняя скобка
+            os.remove(restore_path)
+
+    def generate_name(self, seed=time()):                # выравнивание-отступы и отсутствует ':'
+        return hashlib.md5(str(seed)).hexdigest()
 
 
 def parse_arguments():
@@ -54,14 +57,14 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    Shuffler = shuffler()
+    shuffler = Shuffler()               # имя экемпляра класса с маленькой а имя класса с заглавной
     if args.subcommand == 'rename':
           if args.output:
-                Shuffler.rename(args.dirname, 'restore.info')
+                shuffler.rename(args.dirname, 'restore.info')
           else:
-                Shuffler.rename(args.dirname, args.output)
+                shuffler.rename(args.dirname, args.output)
     elif args.subcommand == 'restore':
-        Shuffler.restore(args.dirname, args.restore_map)
+        shuffler.restore(args.dirname, args.restore_map)
     else:
         sys.exit()
 
