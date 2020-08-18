aed6377ed6415595fdfa63f64f4c5b05b69f7721
RUSLAN AULOV <57061175+Rusbrus@users.noreply.github.com>
2020-08-18 18:12:00 +0300

    Update 4lec2.py

--- Practice/aulov_r/4lec2.py
+++ Practice/aulov_r/4lec2.py
@@ -1,4 +1,3 @@
 num = input('число пожалуйста: ')
-a = str(num)
-for i in range(len(a)):
-    print(f"{i+1} число будет = {a[i]}")
+for i in range(len(num)):
+    print(f"{i+1} число будет = {num[i]}")
