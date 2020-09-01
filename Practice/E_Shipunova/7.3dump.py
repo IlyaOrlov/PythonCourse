42c1814694356031abaf21cba96cc0f96bf84e52
Kate-Shipunova <57718887+Kate-Shipunova@users.noreply.github.com>
2020-09-01 22:21:40 +0300

    Update 7.3dump.py

--- Practice/E_Shipunova/7.3dump.py
+++ Practice/E_Shipunova/7.3dump.py
@@ -1,19 +1,7 @@
 import pickle
 from random import randint as rand
 from random import choice
-
-
-class Human:
-
-   def __init__(self, first_name, last_name, age, position, phone):
-       self.first_name = first_name
-       self.last_name = last_name
-       self.age = age
-       self.position = position
-       self.phone = phone
-
-   def __iter__(self):
-       return iter(self)
+from libHuman import Human
 
 
 def create_human(number):
