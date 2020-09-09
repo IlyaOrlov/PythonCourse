e94d78c5ec46a550753b64c60b518fbdc16f1663
RUSLAN AULOV <57061175+Rusbrus@users.noreply.github.com>
2020-09-09 18:07:09 +0300

    Update 7lec1.py

--- Practice/aulov_r/7lec1.py
+++ Practice/aulov_r/7lec1.py
@@ -1,12 +1,20 @@
-import datetime
+from _datetime import datetime,date
 
-a = input('Первая дата (гггг-мм-дд): ')
-b = input('Вторая дата (гггг-мм-дд): ')
-a = a.split('-')
-b = b.split('-')
-aa = datetime.date(int(a[0]), int(a[1]), int(a[2]))
-bb = datetime.date(int(b[0]), int(b[1]), int(b[2]))
-cc = aa - bb
-print(cc)  # output days and time
-dd = str(cc)
-print(dd.split()[0])  # only days
+a = input('Entered date гггг,мм,дд :')
+a = a.split(',')
+a = date(int(a[0]), int(a[1]), int(a[2]))
+b = input('Entered date гггг,мм,дд :')
+b = b.split(',')
+b = date(int(b[0]), int(b[1]), int(b[2]))
+cc = ((b - a).days)
+w = datetime.weekday(a)
+working_day = []
+for i in range(cc + 1):
+    if w < 5:
+        working_day.append(w)
+        w += 1
+    elif w > 5:
+        w = 0
+    else:
+        w += 1
+print('Number of working day ' + str(len(working_day)))
