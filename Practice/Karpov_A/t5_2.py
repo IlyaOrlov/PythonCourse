cfa5047517a6b5141805de7eb3ae6b3d442e7ff6
cool07 <57254336+cool07@users.noreply.github.com>
2020-09-09 18:49:50 +0300

    Update t5_2.py

--- Practice/Karpov_A/t5_2.py
+++ Practice/Karpov_A/t5_2.py
@@ -1,20 +1,15 @@
 import time
 import random
+from t5_1 import Man
 
-class Man:
+class Pupil(Man):
     def __init__(self, name):
         self.name = name
 
     def solve_task(self):
-        print("I'm not ready yet")
-
-Tom = Man('Tom')
-print(Tom.solve_task())
-
-class Pupil(Man):
-    def solve_task(self):
         time.sleep(random.randint(3,6))
-        print("I'm not ready yet")
+        print("I'm not ready ")
 
 Tom = Pupil('Tom')
-print(Tom.solve_task())
+print(f"{Tom.name}:", end=" ")
+Tom.solve_task()
