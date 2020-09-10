f2cdb51cae5cceac4be8f2db5afa76897762471b
ViktorGa <57584117+ViktorGa@users.noreply.github.com>
2020-09-10 18:59:39 +0300

    import Man

--- Practice/ViktorGa/L5T2.py
+++ Practice/ViktorGa/L5T2.py
@@ -1,13 +1,6 @@
 from random import randint
 from time import sleep
-
-
-class Man:
-    def __init__(self, name):
-        self.name = name
-
-    def solve_task(self):
-        print("I'm not ready yet")
+from L5T1 import Man
 
 
 class Pupil(Man):
