e94834dcec8bd007f72b93588b287fa62f9af5b0
Syatojkin_Evgenij <Syatojkin_Evgenij@mail.ru>
2020-09-11 19:36:58 +0300

    Point_3
    .

--- Practice/EvgeniySyatoykin/2.1.py
+++ Practice/EvgeniySyatoykin/2.1.py
@@ -1,15 +1,7 @@
-<<<<<<< Updated upstream
 def fun(a, b):
 
-    if a > b:
+    if (a > b):
         print(f"max number={a}")
     else:
         print(f"max number{b}")
-=======
-def fun(c, d):
 
-    if (c > d):
-        print(f"max number={c}")
-    else:
-        print(f"max number{d}")
->>>>>>> Stashed changes
