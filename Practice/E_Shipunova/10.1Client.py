409c13ff123701c881d574694b394ffae5820a3c
Kate-Shipunova <57718887+Kate-Shipunova@users.noreply.github.com>
2020-09-18 21:26:06 +0300

    Update 10.1Client.py

--- Practice/E_Shipunova/10.1Client.py
+++ Practice/E_Shipunova/10.1Client.py
@@ -5,7 +5,7 @@ port = 12345
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
-client.send('.$*P$q:*-)%;,$!#:$!:>!'.encode())  # for server
+client.send('.$*P$q:*-)%,$!#:$!:>!'.encode())  # for testing incorrect shifr: '.$*PB$q:*-$!:>!'.encode()
 print(f'I got "{client.recv(1024).decode()}" from server.')
 
 client.close()
