d334d8e7ae50f81a42f4e2a4e7dcf3c726c99072
AlexIvan-Shulc <57383446+AlexIvan-Shulc@users.noreply.github.com>
2020-09-20 19:04:36 +0300

    Update lec4-zad2.py

--- Practice/IvanovA/lec4-zad2.py
+++ Practice/IvanovA/lec4-zad2.py
@@ -1,13 +1,8 @@
 while True:
-    n = input("Введите пятизначное число: ")
-    if n.isdigit() == 0:
-        print("Ошибка введите ещё раз")
-    elif len(n) != 5:
-        print("Ошибка введите ещё раз")
-    elif n[0] == '0':
+    n = input("Введите число: ")
+   if not n.isdigit():
         print("Ошибка введите ещё раз")
     else:
         break
-
 for i in range(len(n)):
     print("{} число равно {}".format(i+1, n[i]))
