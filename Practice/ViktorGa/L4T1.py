133284befe98da3376b21e076bbf8260f08e942f
ViktorGa <57584117+ViktorGa@users.noreply.github.com>
2020-10-05 21:00:32 +0300

    Update L4T1.py

--- Practice/ViktorGa/L4T1.py
+++ Practice/ViktorGa/L4T1.py
@@ -1,11 +1,11 @@
 def fizz_buzz():
     for i in range(1, 100):
         if i % 15 == 0:
-            print("Fizz")
+            print("FizzBuzz")
         elif i % 5 == 0:
             print("Buzz")
         elif i % 3 == 0:
-            print("FizzBuzz")
+            print("Fizz")
         else:
             print(i)
 
