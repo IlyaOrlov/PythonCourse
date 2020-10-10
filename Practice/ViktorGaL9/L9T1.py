69abc0c66038a7c1bda48f759958be255f041164
ViktorGa <57584117+ViktorGa@users.noreply.github.com>
2020-10-10 19:23:21 +0300

    Update L9T1.py

--- Practice/ViktorGaL9/L9T1.py
+++ Practice/ViktorGaL9/L9T1.py
@@ -15,27 +15,27 @@ def find_primes(start, end):
     return lst
 
 
-start_time = time.time()
-find_primes(3, 10000)
-find_primes(10001, 20000)
-find_primes(20001, 30000)
-work_time = time.time() - start_time
-print(work_time)
-
-start_time = time.time()
-threads = [threading.Thread(target=find_primes, args=(3, 10000)),
-           threading.Thread(target=find_primes, args=(10001, 20000)),
-           threading.Thread(target=find_primes, args=(20001, 30000))]
-for thread in threads:
-    thread.start()
-for thread in threads:
-    thread.join()
-work_time = time.time() - start_time
-print(work_time)
+if __name__ == "__main__":
+    
+    start_time = time.time()
+    find_primes(3, 10000)
+    find_primes(10001, 20000)
+    find_primes(20001, 30000)
+    work_time = time.time() - start_time
+    print(work_time)
+    
+    start_time = time.time()
+    threads = [threading.Thread(target=find_primes, args=(3, 10000)),
+             threading.Thread(target=find_primes, args=(10001, 20000)),
+            threading.Thread(target=find_primes, args=(20001, 30000))]
+    for thread in threads:
+       thread.start()
+    for thread in threads:
+       thread.join()
+    work_time = time.time() - start_time
+    print(work_time)
 #Если не сделать thread.start(), то не запустятяся потоки, выдает ошибку.
 #Если не сделать thread.join(), то он неправильно посчитает время, т.к. уже не ждет завершения.
-
-if __name__ == "__main__":
 
     start_time = time.time()
     processes = [multiprocessing.Process(target=find_primes, args=(3, 10000)),
