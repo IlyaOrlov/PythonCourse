48e8b11bbe14077c2a071955ad0a644776aa0393
Kate-Shipunova <57718887+Kate-Shipunova@users.noreply.github.com>
2020-10-13 18:01:10 +0300

    Update 11.1.py

--- Practice/E_Shipunova/11.1.py
+++ Practice/E_Shipunova/11.1.py
@@ -38,72 +38,65 @@ class WrapperSQLite:
 
     def __init__(self, name_db):                                  # for any db
         self.name_db = name_db
+        self.conn = None
 
-    def crud_data(self, command: str) -> str:
-        '''
-        The function return json-format only command is 'SELECT',
-        else return information line.
-        :param command: str
-        :return: str
-        '''
+    def __enter__(self):                                          # open the db
+        self.conn = sqlite3.connect(self.name_db)                 # initialization of self.conn
+        return self                                               # return an instant of the class for later call
 
-        if len(command) != 0:                                     # checking the command
-            check_com = command.lower()
-        else:
-            return 'Command is empty!'
+    def __exit__(self, exc_type, exc_val, exc_tb):                # close the db
+        self.conn.close()
 
-        if ('insert' not in check_com) and ('select' not in check_com) and ('update' not in check_com) and ('delete' not in check_com):
-            return 'Incorrect command!'
+    def execute_ins_upd_del(self, command: str) -> str:
+        if self.check_command(command):
+            self.conn.execute(command)
+            self.conn.commit()
+            return 'Changes saved.'
 
-        conn = sqlite3.connect(self.name_db)
+        return 'Incorrect command!'
 
-        if 'insert' in check_com:
-            conn.execute(command)
-            conn.commit()
-            conn.close()
-
-            return 'Data added.'
-
-        elif 'select' in check_com:
+    def execute_select(self, command: str) -> str:
+        if self.check_command(command):
             arr_json = []
-            cursor = conn.execute(command)
+            cursor = self.conn.execute(command)
+            name_row = [description[0] for description in cursor.description]  # for get name of row
+            result = cursor.fetchall()                                         # for get result of select
 
-            name_row = [description[0] for description in cursor.description]   # for get name of row
-            result = cursor.fetchall()                                          # for get result of select
-
-            for row in result:                                                  # made json-format
+            for row in result:                                                 # made json-format
                 buf = dict()
                 for i in range(len(name_row)):
                     buf[name_row[i]] = row[i]
                 arr_json.append(buf)
-            conn.commit()
-            conn.close()
 
-            return json.dumps(arr_json, sort_keys=True, indent=4)               # for pretty result
+            self.conn.commit()
+            return json.dumps(arr_json, sort_keys=True, indent=4)              # for pretty result
 
-        elif 'update' in check_com:
-            conn.execute(command)
-            conn.commit()
-            conn.close()
+        return 'Incorrect command!'
 
-            return 'Data updated.'
+    @staticmethod                                             # class and instance attributes aren't used - log.connect
+    def check_command(command: str) -> bool:
 
-        conn.execute(command)                                                   # if command is 'delete'
-        conn.commit()
-        conn.close()
+        if len(command) != 0:                                 # checking the command
+            comm = command.lower()
+        else:
+            return False
 
-        return 'Data deleted.'
+        if ('insert' not in comm) and ('select' not in comm) and ('update' not in comm) and ('delete' not in comm):
+            return False
+
+        return True
 
 
 if __name__ == '__main__':
     if create_db_with_persons_tab('persons.db'):
-        wrap = WrapperSQLite('persons.db')
-        print(wrap.crud_data('INSERT INTO persons (id, name, age) VALUES (8, "David", 44);'))  # position == None
-        print(wrap.crud_data('UPDATE persons SET age = 100 WHERE id = 1;'))
-        print(wrap.crud_data('DELETE FROM persons WHERE age = 100 AND name = "Noah";'))        # where id = 1
-        print(wrap.crud_data('SELECT id, name, age FROM persons WHERE position IS NULL'))      # where id = 8
-        print(wrap.crud_data('SELECT * FROM persons WHERE age = 22 OR age = 24 ORDER BY age'))
-        print(wrap.crud_data(''))
-        print(wrap.crud_data('Incorrect data'))
+        with WrapperSQLite('persons.db') as wrap:             # wrap is instant of the class for later call
+            print(wrap.execute_ins_upd_del('INSERT INTO persons (id, name, age) VALUES (8, "David", 44);'))  # pos==None
+            print(wrap.execute_ins_upd_del('UPDATE persons SET age = 100 WHERE id = 1;'))
+            print(wrap.execute_ins_upd_del('DELETE FROM persons WHERE age = 100 AND name = "Noah";'))       # where id=1
+            print(wrap.execute_select('SELECT id, name, age FROM persons WHERE position IS NULL'))          # where id=8
+            print(wrap.execute_select('SELECT * FROM persons WHERE age = 22 OR age = 24 ORDER BY age'))
+            print(wrap.execute_ins_upd_del(''))
+            print(wrap.execute_ins_upd_del('Incorrect data'))
+
         if delete_db('persons.db'):
-            print('File of db deleted')
+            print('File of db deleted.')
