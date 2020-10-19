7cb10bc2fbf28a5c981b2705d2e9fa085d1a0c51
Mikle <57234879+MikhailTreushnikov@users.noreply.github.com>
2020-10-19 21:19:47 +0300

    Lab2 + Lab3 + Lab4 + Lab5 + Lab6 + Lab7 (#267)
    * Lab2 + Lab3 + Lab4
    
    * Delete .DS_Store
    
    * added gitignore file
    
    * Fixes bags and Lab5 + Lab6
    
    * Deleted gitignore file
    
    * fixed 3.2
    
    * deleted DS_Store
    
    * fixed bags and added new lab 7

---
+++ Practice/Mihail_Treushnikov/pupil.py
@@ -0,0 +1,15 @@
+import random
+from time import sleep
+from man import Man
+
+class Pupil(Man):
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def solve_task(self):
+        sleep(random.randint(3,6))
+        super()
+
+man = Pupil('Mike')
+man.solve_task()
