23ade8a4e94ae373499334316413cd3e2b5c1c36
SemKilyaev <darkvanted@gmail.com>
2020-10-20 15:56:42 +0300

    Fixed

--- Practice/Sam_Kilyaev/Lection_11_Task_1.py
+++ Practice/Sam_Kilyaev/Lection_11_Task_1.py
@@ -4,30 +4,33 @@ import json
 
 
 def make_data_base(name_data_base):
-    conn = sqlite3.connect(name_data_base)
-    print('Data base created')
-    conn.execute('CREATE TABLE PLAYERS'
-                 ' (ID INT PRIMARY KEY NOT NULL,'
-                 '  NAME TEXT NOT NULL,'
-                 '  AGE INT NOT NULL,'
-                 '  COUNTRY TEXT NOT NULL,'
-                 '  PLATFORM TEXT NOT NULL);')
-    print('Table created')
-    conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
-                 'VALUES (1, "Jon", 25, "Great Britain", "PS4");')
-    conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
-                 'VALUES (2, "Brus", 36, "Norway", "XBOX ONE");')
-    conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
-                 'VALUES (3, "Mary", 28, "USA", "PC");')
-    conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
-                 'VALUES (4, "Helen", 23, "France", "PS4");')
-    conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
-                 'VALUES (5, "Sergey", 31, "Russia", "PC");')
-    conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
-                 'VALUES (6, "Shan Chao", 19, "China", "PS4");')
-    conn.commit()
-    conn.close()
-    return os.path.isfile(name_data_base)
+    if os.path.isfile(name_data_base):
+        return True
+    else:
+        conn = sqlite3.connect(name_data_base)
+        print('Data base created')
+        conn.execute('CREATE TABLE PLAYERS'
+                     ' (ID INT PRIMARY KEY NOT NULL,'
+                     '  NAME TEXT NOT NULL,'
+                     '  AGE INT NOT NULL,'
+                     '  COUNTRY TEXT NOT NULL,'
+                     '  PLATFORM TEXT NOT NULL);')
+        print('Table created')
+        conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
+                     'VALUES (1, "Jon", 25, "Great Britain", "PS4");')
+        conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
+                     'VALUES (2, "Brus", 36, "Norway", "XBOX ONE");')
+        conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
+                     'VALUES (3, "Mary", 28, "USA", "PC");')
+        conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
+                     'VALUES (4, "Helen", 23, "France", "PS4");')
+        conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
+                     'VALUES (5, "Sergey", 31, "Russia", "PC");')
+        conn.execute('INSERT INTO PLAYERS (ID, NAME, AGE, COUNTRY, PLATFORM)'
+                     'VALUES (6, "Shan Chao", 19, "China", "PS4");')
+        conn.commit()
+        conn.close()
+        return 'Data base created'
 
 
 def delete_db(name_data_base):
@@ -60,7 +63,6 @@ class SQLiteClass:
             for i in range(len(name_row)):
                 buf[name_row[i]] = row[i]
             arr_json.append(buf)
-        self.conn.commit()
         return json.dumps(arr_json, sort_keys=True, indent=4)
 
 
