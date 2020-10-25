20dd7ce242b37e6341f51bc9d3257f0649a26046
asincov <sashasintsov@mail.ru>
2020-10-25 20:41:04 +0300

    task_3 changed

--- Practice/sintsov/pre_exam_2/pre_exam_2_tasks_1_3.py
+++ Practice/sintsov/pre_exam_2/pre_exam_2_tasks_1_3.py
@@ -15,30 +15,41 @@ def mysum(a,b):
 def str_multyply(s, n=0):
     print(s*n)
 
+def print_multydict(*args, **kwargs):
+    for key, value in kwargs.items():
+        for arg in args:
+            print('key = {} , value * arg = {}'.format(key, value * arg))
+
+
 class FunRanner:
     def __init__(self):
         self.fun_list = []
-        self.__d = {}
+        self.__args = {}
+        self.__kwargs = {}
 
-    def add_fun(self, fun, *args):
+    def add_fun(self, fun, *args, **kwargs):
         self.fun_list.append(fun)
-        self.__d[fun] = args
+        self.__args[fun] = args
+        self.__kwargs[fun] = kwargs
 
     def remove_fun(self, fun):
         self.fun_list.remove(fun)
-        del self.__d[fun]
+        del self.__args[fun]
+        del self.__kwargs[fun]
 
     def run_all(self):
-        for fun in self.__d.keys():
-            fun(*self.__d.get(fun))
+        for fun in self.fun_list:
+            fun(*self.__args.get(fun), **self.__kwargs.get(fun))
 
     def remove_all(self):
         self.fun_list = []
-        self.__d.clear()
+        self.__args.clear()
+        self.__kwargs.clear()
 
 f = FunRanner()
 f.add_fun(mysum, 1, 2)
 f.add_fun(str_multyply, 'a', 5)
+f.add_fun(print_multydict, 3, 5, a=1, b=5)
 f.run_all()
 f.remove_fun(mysum)
 f.run_all()
