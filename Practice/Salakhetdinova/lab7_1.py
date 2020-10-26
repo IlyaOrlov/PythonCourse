3d2e1953da57f56da87801abfdab3b1a3ce38151
Oigul <aj3913@yandex.ru>
2020-10-26 20:11:55 +0300

    +lec8

--- Practice/Salakhetdinova/lab7_1.py
+++ Practice/Salakhetdinova/lab7_1.py
@@ -1,17 +1,17 @@
 import datetime
 
 def day_works(date1, date2):
-    delta = date1 - date2
     date = date1
     work_day = 0
 
-    for i in range(int(delta.days) + 1):
+    while date <= date2:
         if date.weekday() < 5:
             work_day += 1
         date += datetime.timedelta(days=1)
     return work_day
 
+
 if __name__ == "__main__":
-    date1 = datetime.datetime(2020, 10, 15)
-    date2 = datetime.datetime(2020, 10, 1)
+    date1 = datetime.datetime(2020, 10, 5)
+    date2 = datetime.datetime(2020, 10, 16)
     print(day_works(date1, date2))
