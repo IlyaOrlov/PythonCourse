94dcdf7c4381ab496b8ffad73236881f1b695d00
Oigul <57235051+Oigul@users.noreply.github.com>
2020-10-27 18:04:03 +0300

    Update lab4_3.py

--- Practice/Salakhetdinova/lab4_3.py
+++ Practice/Salakhetdinova/lab4_3.py
@@ -6,24 +6,24 @@ def sort(arr):
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[min_value]:
                 min_value = j
-<<<<<<< HEAD
+ HEAD
                 arr[i] = arr[min_value]
                 arr[min_value] = arr[i]
-=======
+
         temp = arr[i]
         arr[i] = arr[min_value]
         arr[min_value] = temp
 
             
->>>>>>> c8db6ed0c1733acc791023d6cbb9cab97186a5ae
+ c8db6ed0c1733acc791023d6cbb9cab97186a5ae
 
 arr = []
 for i in range(1, 7):
     arr.append(random.randint(1, 50))
 print(arr)
 sort(arr)
-<<<<<<< HEAD
+ HEAD
 print(arr)
-=======
+
 print(arr)
->>>>>>> c8db6ed0c1733acc791023d6cbb9cab97186a5ae
+ c8db6ed0c1733acc791023d6cbb9cab97186a5ae
