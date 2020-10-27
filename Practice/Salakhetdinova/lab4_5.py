41e243046c6f62b40326a57c0609e2c0231caf1c
Oigul <57235051+Oigul@users.noreply.github.com>
2020-10-27 18:24:41 +0300

    Update lab4_5.py

--- Practice/Salakhetdinova/lab4_5.py
+++ Practice/Salakhetdinova/lab4_5.py
@@ -1,19 +1,9 @@
 def interpolate(string, dictionary):
-<<<<<<< HEAD
     for key in dictionary:
-        if key in string:
-            string = string.replace(key, dictionary[key])
-=======
-    for key in dictionary:       
-        string = string.replace(key, dictionary[key])
->>>>>>> c8db6ed0c1733acc791023d6cbb9cab97186a5ae
+       string = string.replace(key, dictionary[key])
     return string
 
 string = "Люди только чай пьют, а в их душах совершается трагедия"
 dictionary = {'чай': 'кофе', 'трагедия': 'комедия'}
 print(string)
-<<<<<<< HEAD
 print(interpolate(string, dictionary))
-=======
-print(interpolate(string, dictionary))
->>>>>>> c8db6ed0c1733acc791023d6cbb9cab97186a5ae
